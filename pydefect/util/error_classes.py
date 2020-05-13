24cdc89d68b17f936f1d0909f3946a5ea6b9973c
yuuukuma <yuuukuma@gmail.com>
2020-05-13 21:58:20 +0900

    Add PydefectError and NoSupercellError to error_classes.py

---
+++ pydefect/util/error_classes.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+
+
+class PydefectError(Exception):
+    pass
+
+
+class NoSupercellError(PydefectError):
+    pass
