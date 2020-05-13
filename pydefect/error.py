651ea04256bfa8da95dd9bb41d0ebad12748eae9
yuuukuma <yuuukuma@gmail.com>
2020-05-13 15:55:22 +0900

    Create error.py and version.py

---
+++ pydefect/error.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+
+
+class PydefectError(Exception):
+    pass
---
+++ pydefect/version.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+__version__ = "0.0.1"
