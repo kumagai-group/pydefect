6ed32c1ec1b8cc32dd86de1d6c073820c047d8b0
yuuukuma <yuuukuma@gmail.com>
2020-05-15 08:07:22 +0900

    Add Distances to structure_tools.py

---
+++ pydefect/tests/util/test_structure_tools.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+
+import pytest
+import numpy as np
+
+from pydefect.util.structure_tools import Distances
+from pymatgen import Lattice, Structure
+
+
+@pytest.fixture
+def monoclinic_supercell():
+    lattice = Lattice.orthorhombic(5, 6, 7)
+    coords = [
+        [0.0, 0.0, 0.0],
+        [0.5, 0.5, 0.0],
+        [0.5, 0.0, 0.5],
+        [0.0, 0.5, 0.5],
+
+        [0.0, 0.0, 0.5],
+        [0.0, 0.5, 0.0],
+        [0.5, 0.0, 0.0],
+        [0.5, 0.5, 0.5],
+              ]
+    return Structure(lattice=lattice, species=["H"] * 4 + ["He"] * 4,
+                     coords=coords)
+
+
+def test_distances(monoclinic_supercell):
+    distances = Distances(monoclinic_supercell, coord=[0.25, 0.25, 0.25])
+    np.testing.assert_almost_equal(distances.distances(), [5.2440442 / 2] * 8)
+
+    distances = Distances(monoclinic_supercell, coord=[0.5, 0.5, 0.5])
+    expected = [5.2440442, 3.5, 3.0, 2.5, 3.9051248, 4.3011626, 4.6097722]
+    np.testing.assert_almost_equal(distances.distances(), expected)
+
+
+def test_shortest_distances(monoclinic_supercell):
+    distances = Distances(monoclinic_supercell, coord=[0.5, 0.5, 0.5])
+    assert distances.shortest_distance == 2.5
+
+
+def test_coord_distances(monoclinic_supercell):
+    distances = Distances(monoclinic_supercell, coord=[0.5, 0.5, 0.5])
+    cutoff_distance_factor = 3.9051248 / 2.5 + 1e-5
+
+    actual = distances.coord_distances(cutoff_distance_factor)
+    expected = {"H": [2.5, 3.0, 3.5], "He": [3.91]}
+    assert actual == expected
+
+
+"""
+TODO
+
+DONE
+- Return distances to atoms in the given structure from the given coord.
+- Return the shortest distance from the given coord to atoms
+- Return the coordination distances from the given coord to atoms
+  The multiplication_factor to the shortest distance is given.
+"""
---
+++ pydefect/util/structure_tools.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+from typing import List
+import numpy as np
+from collections import defaultdict
+from pymatgen import Structure
+
+
+class Distances:
+    def __init__(self,
+                 structure: Structure,
+                 coord: np.array):
+        self.structure = structure
+        self.coord = coord
+
+    def distances(self, remove_self=True):
+        result = []
+        for frac_coord in self.structure.frac_coords:
+            lattice = self.structure.lattice
+            distance, _ = lattice.get_distance_and_image(frac_coord, self.coord)
+            if remove_self and distance < 1e-5:
+                continue
+            result.append(distance)
+
+        return result
+
+    @property
+    def shortest_distance(self):
+        return min(self.distances())
+
+    def coord_distances(self, cutoff_distance_factor):
+        cutoff = self.shortest_distance * cutoff_distance_factor
+
+        elements = [element.specie.name for element in self.structure]
+        e_d = zip(elements, self.distances(False))
+
+        unsorted_distances = defaultdict(list)
+        for element, distance in filter(lambda x: 1e-5 < x[1] < cutoff, e_d):
+            unsorted_distances[element].append(round(distance, 2))
+
+        result = {}
+        for element, distances in unsorted_distances.items():
+            result[element] = sorted(distances)
+
+        return result
