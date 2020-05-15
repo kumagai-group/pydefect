4fb9252783a895a99001e45fe5ed31f7aba16e88
yuuukuma <yuuukuma@gmail.com>
2020-05-15 11:50:38 +0900

    Add DefectName in defect_name.py

---
+++ pydefect/input_maker/defect_name.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+
+from dataclasses import dataclass
+import re
+from typing import Union
+
+from monty.json import MSONable
+
+
+@dataclass()
+class DefectName(MSONable):
+    in_atom: str
+    out_atom: str
+    charge: int
+
+    def __str__(self):
+        return "_".join([self.in_atom, self.out_atom, str(self.charge)])
+
+    def __hash__(self):
+        return hash(str(self))
+
+    @classmethod
+    def from_str(cls, string) -> "DefectName":
+        name = string.split("_")
+        assert len(name) == 3
+        return cls(in_atom=name[0], out_atom=name[1], charge=int(name[2]))
+
