15cf510a56f08de08c082e4b11fa7e86a69d2664
yuuukuma <yuuukuma@gmail.com>
2020-05-16 09:54:13 +0900

    Add _create_substitutional_set to DefectSetMaker in defect_set.py

--- pydefect/input_maker/defect_set.py
+++ pydefect/input_maker/defect_set.py
@@ -3,6 +3,7 @@
 from typing import List, Dict
 from pathlib import Path
 from copy import deepcopy
+from itertools import product
 
 from pydefect.input_maker.defect_name import DefectName
 from pydefect.input_maker.supercell_info import SupercellInfo
@@ -28,15 +29,25 @@ class DefectSetMaker:
     def __init__(self,
                  supercell_info: SupercellInfo,
                  overwritten_oxi_states: Dict[str, int] = None,
-                 dopants: List[str] = None):
+                 dopants: List[str] = None,
+                 ele_neg_diff: float = 2.0):
         self.supercell_info = supercell_info
-        self._oxidation_states = deepcopy(oxidation_state)
-        if overwritten_oxi_states:
-            self._oxidation_states.update(overwritten_oxi_states)
-
-        self._dopants = dopants
+        self._overwritten_oxi_states = overwritten_oxi_states or {}
+        self._dopants = dopants or []
+        self._ele_neg_diff = ele_neg_diff
 
         self.defect_set = self._create_defect_set()
+
+    def _oxidation_state(self, element):
+        try:
+            return self._overwritten_oxi_states[element]
+        except KeyError:
+            return oxidation_state(element)
+
+    @property
+    def _host_elements(self):
+        species_set = set(self.supercell_info.structure.species)
+        return [str(specie) for specie in species_set]
 
     def _create_defect_set(self):
         result = self._create_vacancy_set()
@@ -46,22 +57,32 @@ class DefectSetMaker:
     def _create_vacancy_set(self):
         result = []
         for name, site in self.supercell_info.sites.items():
-            ox_state = self._oxidation_states[site.element]
-            for charge in charge_set(ox_state):
-
+            oxi_state = self._oxidation_state(site.element)
+            for charge in charge_set(oxi_state):
                 result.append(DefectName("Va", name, charge))
+
         return result
 
     def _create_substitutional_set(self):
-        print(electronegativity["Li"])
-        print(electronegativity["H"])
-        print(electronegativity["He"])
-#        for dopant
+        result = []
+        subs = self._dopants + self._host_elements
+        for sub_element, (name, site) \
+                in product(subs, self.supercell_info.sites.items()):
+            host_element = site.element
+            sub_en = electronegativity(sub_element)
+            host_en = electronegativity(host_element)
+            are_same_elem = sub_element == host_element
+            if None in (sub_en, host_en) or are_same_elem or \
+                    abs(sub_en - host_en) > self._ele_neg_diff:
+                continue
 
-        return [        DefectName.from_str("Li_H1_0"),
-                        DefectName.from_str("Li_He1_-1"),
-                        DefectName.from_str("Li_He1_0"),
-                        DefectName.from_str("Li_He1_1"),]
+            sub_oxi_state = self._oxidation_state(sub_element)
+            host_oxi_state = self._oxidation_state(host_element)
+            oxi_state_diff = sub_oxi_state - host_oxi_state
+            for charge in charge_set(oxi_state_diff):
+                result.append(DefectName(sub_element, name, charge))
+
+        return result
 
 
 def charge_set(ox_state: int) -> List[int]:
