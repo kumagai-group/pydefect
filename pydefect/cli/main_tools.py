91288e6a3c2b8c6c9fccf04c2ecacc958907903b
yuuukuma <yuuukuma@gmail.com>
2020-05-18 13:15:50 +0900

    Add sanitize_matrix to main_tools.py

---
+++ pydefect/cli/main_tools.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+from typing import List
+
+import numpy as np
+
+
+def sanitize_matrix(matrix: List[int]) -> List[List[int]]:
+
+    if len(matrix) == 9:
+        return [matrix[:3], matrix[3:6], matrix[6:]]
+    elif len(matrix) == 3:
+        result = np.eye(3, dtype=int)
+        for i in range(3):
+            result[i, i] = matrix[i]
+        return result.tolist()
+    elif len(matrix) == 1:
+        result = np.eye(3, dtype=int)
+        for i in range(3):
+            result[i, i] = matrix[0]
+        return result.tolist()
+    else:
+        raise ValueError(f"Matrix element length {len(matrix)} is improper.")
---
+++ pydefect/tests/cli/test_main_tools.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+
+import pytest
+
+from pydefect.cli.main_tools import sanitize_matrix
+
+
+def test_sanitize_matrix_9_input_values():
+    actual = sanitize_matrix(list(range(9)))
+    expected = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    assert actual == expected
+
+
+def test_sanitize_matrix_3_input_values():
+    actual = sanitize_matrix(list(range(1, 4)))
+    expected = [[1, 0, 0], [0, 2, 0], [0, 0, 3]]
+    assert actual == expected
+
+
+def test_sanitize_matrix_1_input_value():
+    actual = sanitize_matrix([4])
+    expected = [[4, 0, 0], [0, 4, 0], [0, 0, 4]]
+    assert actual == expected
+
+
+def test_sanitize_matrix_raise_error_for_improper_matrix_element_length():
+    with pytest.raises(ValueError):
+         sanitize_matrix([1, 4])
+
+
+"""
+TODO
+- return matrix
+
+DONE
+"""
