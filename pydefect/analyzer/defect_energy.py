f575f783cacc5e6af0391a8fe98e74351e204129
yuuukuma <yuuukuma@gmail.com>
2020-05-19 16:05:43 +0900

    Add corrected_energies and CrossPoints in defect_energy.py

--- pydefect/analyzer/defect_energy.py
+++ pydefect/analyzer/defect_energy.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
 from dataclasses import dataclass
-from typing import List, Dict
+from typing import List
 
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import HalfspaceIntersection
 
@@ -10,16 +11,19 @@ from scipy.spatial import HalfspaceIntersection
 @dataclass
 class DefectEnergy:
     name: str
-    num_atom_change: Dict[str, int]
     charges: List[int]
     energies: List[float]
     corrections: List[float]
     shallow: List[bool]
 
+    @property
+    def corrected_energies(self):
+        return [e + c for e, c in zip(self.energies, self.corrections)]
+
     def cross_points(self, ef_min, ef_max):
         large_minus_number = -1e4
         half_spaces = []
-        for charge, energy in zip(self.charges, self.energies):
+        for charge, energy in zip(self.charges, self.corrected_energies):
             half_spaces.append([-charge, 1, -energy])
 
         half_spaces.append([-1, 0, ef_min])
@@ -41,15 +45,64 @@ class DefectEnergy:
         all_points = boundary_points + inner_cross_points
 
         return (sorted(all_points, key=lambda v: v[0]),
-                sorted(inner_cross_points, key=lambda v: v[0]))
+                sorted(inner_cross_points, key=lambda v: v[0]),
+                sorted(boundary_points, key=lambda v: v[0]))
+
+
+@dataclass
+class CrossPoints:
+    inner_cross_points: List[List[float]]
+    boundary_points: List[List[float]]
+
+    @property
+    def all_points(self):
+        return sorted(self.boundary_points + self.inner_cross_points,
+                      key=lambda v: v[0])
+
+    @property
+    def t_inner_cross_points(self):
+        return np.transpose(np.array(self.inner_cross_points)).tolist()
+
+    @property
+    def t_boundary_points(self):
+        return np.transpose(np.array(self.boundary_points)).tolist()
 
 
 @dataclass
 class DefectEnergies:
     defect_energies: List[DefectEnergy]
-    chempot: Dict[str, int]
-    keywords: List[str]
     vbm: float
     cbm: float
     supercell_vbm: float
     supercell_cbm: float
+
+    def plot(self):
+        for de in self.defect_energies:
+            all_pts, cross_pts, boundaries = de.cross_points(self.vbm, self.cbm)
+            xs, ys = np.transpose(np.array(all_pts)).tolist()
+            plt.plot(xs, ys)
+            xs, ys = np.transpose(np.array(cross_pts)).tolist()
+            plt.scatter(xs, ys, marker="o")
+
+        return plt
+
+        # # Add points between x_min and x_max
+        # for cp, charges in zip(tl["cross_points"], tl["charges"]):
+        #     if x_min < cp[0] - self.vbm < x_max:
+        #         cross_points.append([cp[0] - self.vbm, cp[1]])
+        #         # need to sort the charge.
+        #         charge_set.add(sorted(charges)[1])
+        #         y_min, y_max = min([cp[1], y_min]), max([cp[1], y_max])
+        #         ax.scatter(x=cp[0] - self.vbm,
+        #                    y=cp[1],
+        #                    marker='o',
+        #                    color=color[i])
+
+        # # Add the plot point at x_max
+        # charge, y = \
+        #     min_e_at_ef(self.defect_energies[name], x_max + self.vbm)
+        # cross_points.append([x_max, y])
+        # charge_set.add(charge)
+        # y_min, y_max = min([y, y_min]), max([y, y_max])
+        # if min(charge_set) > 0:
+        #     ax.plot(x_max, y, marker="o", mec=color[i], mfc="white")
--- pydefect/tests/analyzer/test_defect_energy.py
+++ pydefect/tests/analyzer/test_defect_energy.py
@@ -3,28 +3,56 @@
 
 import pytest
 
-from pydefect.analyzer.defect_energy import DefectEnergy
+from pydefect.analyzer.defect_energy import DefectEnergy, CrossPoints, \
+    DefectEnergies
 
 
 @pytest.fixture
 def defect_energy():
     return DefectEnergy(name="Va_O1",
-                        num_atom_change={"O": -1},
                         charges=[0, 1, 2],
-                        energies=[6, 3, -4],
-                        corrections=[0, 0, 0],
+                        energies=[5, 3, -4],
+                        corrections=[1, 0, 0],
                         shallow=[False, False, False],
                         )
 
 
-def test(defect_energy):
+def test_corrected_energies(defect_energy):
+    assert defect_energy.corrected_energies == [6, 3, -4]
+
+
+def test_defect_energy_cross_points(defect_energy):
     actual = defect_energy.cross_points(1, 6)
-    expected = ([[1.0, -2.0], [5.0, 6.0], [6.0, 6.0]], [[5.0, 6.0]])
+    expected = ([[1.0, -2.0], [5.0, 6.0], [6.0, 6.0]],
+                [[5.0, 6.0]],
+                [[1.0, -2.0], [6.0, 6.0]])
     assert actual == expected
+
+
+def test_cross_points():
+    inner_cross_points = [[2, 20], [3, 30], [4, 40]]
+    boundary_points = [[1, 10], [5, 50]]
+    cross_points = CrossPoints(inner_cross_points, boundary_points)
+
+    assert cross_points.all_points == [[1, 10], [2, 20], [3, 30], [4, 40], [5, 50]]
+    assert cross_points.t_inner_cross_points == [[2, 3, 4], [20, 30, 40]]
+    assert cross_points.t_boundary_points == [[1, 5], [10, 50]]
+
+
+def test_defect_energies(defect_energy):
+    defect_energies = DefectEnergies([defect_energy],
+                                     vbm=1.0,
+                                     cbm=6.0,
+                                     supercell_vbm=1.5,
+                                     supercell_cbm=5.5)
+    plt = defect_energies.plot()
+    plt.show()
 
 
 """
 TODO
+- Correction.
+
 - Evaluate the crossing points at given Fermi level range.
 
 - Draw a single defect
