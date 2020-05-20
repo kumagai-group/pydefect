3eb9eb764fdb84a3d51f2e4b6d30657dea184cb9
yuuukuma <yuuukuma@gmail.com>
2020-05-20 16:25:51 +0900

    Rename ChemPotDiagInfo to ChemPotDiag in chem_pot_diag.py

--- pydefect/chem_pot_diag/chem_pot_diag.py
+++ pydefect/chem_pot_diag/chem_pot_diag.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
-
 from typing import Dict
 
+import numpy as np
 from pymatgen import Composition
+from scipy.spatial.qhull import HalfspaceIntersection
 
 
-class ChemPotDiagInfo:
+class ChemPotDiag:
     def __init__(self, energies: Dict[Composition, float]):
         self.abs_energies = {c: e / c.num_atoms for c, e in energies.items()}
         self.vertex_elements = self._get_vertices()
+        self.dim = len(self.vertex_elements)
         self.offset_to_abs = self._get_offset_to_abs()
         self.rel_energies = self._get_rel_energies()
         self.vertex_coords = self._get_vertex_coords()
@@ -30,15 +32,35 @@ class ChemPotDiagInfo:
     def _get_rel_energies(self):
         result = {}
         for c, e in self.abs_energies.items():
-            frac = [c.fractional_composition[e] for e in self.vertex_elements]
-            sub = sum(f * offset for f, offset in zip(frac, self.offset_to_abs))
+            sub = sum(f * offset for f, offset
+                      in zip(self._frac_composition(c), self.offset_to_abs))
             result[c] = e - sub
 
         return result
 
-    def _get_vertex_coords(self):
+    def _frac_composition(self, c):
+        return [c.fractional_composition[e] for e in self.vertex_elements]
 
-        result = [[-0.5, 0.0], [0.0, -1.0]]
+    def _get_vertex_coords(self):
+        large_minus_number = -1e5
+        half_spaces = []
+        for c, e in self.rel_energies.items():
+            half_spaces.append(self._frac_composition(c) + [-e])
+
+        for i in range(self.dim):
+            x = [0.0] * self.dim
+            x[i] = -1.0
+            x.append(large_minus_number)
+            half_spaces.append(x)
+
+        feasible_point = np.array([large_minus_number + 1] * self.dim)
+        hs = HalfspaceIntersection(np.array(half_spaces), feasible_point)
+
+        result = []
+        for intersection in hs.intersections:
+            if min(intersection) != large_minus_number:
+                result.append(intersection)
+
         return result
 
 
--- pydefect/tests/chem_pot_diag/test_chem_pot_diag.py
+++ pydefect/tests/chem_pot_diag/test_chem_pot_diag.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
 
+import numpy as np
 from pymatgen import Composition, Element
 
-from pydefect.chem_pot_diag.chem_pot_diag_info import ChemPotDiagInfo
+from pydefect.chem_pot_diag.chem_pot_diag import ChemPotDiag
 
 # When same keys are inserted, only the latter one is accepted.
 energies = {Composition("H"): 0.0,
@@ -13,11 +14,12 @@ energies = {Composition("H"): 0.0,
 
 
 def test_chem_pot_diag_info():
-    cpd_info = ChemPotDiagInfo(energies)
+    cpd_info = ChemPotDiag(energies)
     assert cpd_info.vertex_elements == [Element("H"), Element("O")]
     assert cpd_info.offset_to_abs == [0.0, 1.0]
     assert cpd_info.rel_energies[Composition("H2O")] == (-2.0 - 1.0) / 3
-    assert cpd_info.vertex_coords == [[-0.5, 0.0], [0.0, -1.0]]
+    np.testing.assert_array_almost_equal(cpd_info.vertex_coords,
+                                         [[0.0, -3.0], [-1.5, 0.0]])
 
 """
 TODO
