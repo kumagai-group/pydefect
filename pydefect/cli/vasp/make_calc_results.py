7a8a054099c5420a78b26c7f30d174eb8ec73d35
yuuukuma <yuuukuma@gmail.com>
2020-05-21 15:10:37 +0900

    Add MakeCalcResultsFromVaspResults to make_calc_results.py

---
+++ pydefect/cli/vasp/make_calc_results.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+from pymatgen.io.vasp import Vasprun, Outcar
+from vise.analyzer.vasp.band_edge_properties import VaspBandEdgeProperties
+from vise.util.structure_symmetrizer import StructureSymmetrizer
+
+from pydefect.analyzer.calc_results import CalcResults
+from pydefect.defaults import defaults
+
+
+class MakeCalcResultsFromVaspResults:
+    def __init__(self, vasprun: Vasprun, outcar: Outcar):
+        structure = vasprun.final_structure
+        magnetization = outcar.total_mag or 0.0
+
+        symmetrizer = StructureSymmetrizer(
+            structure,
+            symprec=defaults.symmetry_length_tolerance,
+            angle_tolerance=defaults.symmetry_angle_tolerance,
+            time_reversal=abs(magnetization) > defaults.integer_criterion)
+        band_edge_prop = VaspBandEdgeProperties(
+            vasprun, outcar, defaults.integer_criterion)
+
+        self.calc_results = CalcResults(
+            structure=structure,
+            site_symmetry=symmetrizer.point_group,
+            energy=outcar.final_energy,
+            magnetization=magnetization,
+            eigenvalues=vasprun.eigenvalues,
+            kpoint_coords=vasprun.actual_kpoints,
+            kpoint_weights=vasprun.actual_kpoints_weights,
+            electrostatic_potential=outcar.electrostatic_potential,
+            vbm_info=band_edge_prop.vbm_info,
+            cbm_info=band_edge_prop.vbm_info,
+            fermi_level=vasprun.efermi,
+            electronic_conv=vasprun.converged_electronic,
+            ionic_conv=vasprun.converged_ionic)
