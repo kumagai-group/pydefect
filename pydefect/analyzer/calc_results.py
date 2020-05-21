fef209c4a23c63a9a09ea014ad84526c5cb0c9c0
yuuukuma <yuuukuma@gmail.com>
2020-05-21 15:10:08 +0900

    Modify the attributes of CalcResults in calc_results.py

--- pydefect/analyzer/calc_results.py
+++ pydefect/analyzer/calc_results.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 
 import numpy as np
 from pymatgen import IStructure
@@ -17,9 +17,9 @@ class CalcResults:
     kpoint_coords: List[List[float]]
     kpoint_weights: List[float]
     electrostatic_potential: List[float]
-    vbm: float
-    cbm: float
-    volume: float
+    vbm_info: float
+    cbm_info: float
     fermi_level: float
-
+    electronic_conv: Optional[bool] = None
+    ionic_conv: Optional[bool] = None
 
