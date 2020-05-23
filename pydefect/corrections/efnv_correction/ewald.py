e0e0a90d1f062d673996cc1ec82053b596f879eb
yuuukuma <yuuukuma@gmail.com>
2020-05-23 18:10:12 +0900

    Add Ewald to ewald.py

--- pydefect/corrections/efnv_correction/ewald.py
+++ pydefect/corrections/efnv_correction/ewald.py
@@ -1,38 +1,112 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
 from dataclasses import dataclass
+from math import sqrt, pow, ceil
 
 import numpy as np
 from monty.json import MSONable
-from pymatgen import Lattice
+from numpy import dot, cross, pi
+from numpy.linalg import norm, inv, det
+from pymatgen import IStructure
+from scipy.stats import mstats
+
+
+def calc_max_sphere_radius(lattice_vectors: np.ndarray) -> float:
+    """ Calculate three distances between two parallel planes using
+        (a_i x a_j) . a_k / |a_i . a_j|
+    """
+    distances = np.zeros(3, dtype=float)
+    for i in range(3):
+        a_i_a_j = cross(lattice_vectors[i - 2], lattice_vectors[i - 1])
+        a_k = lattice_vectors[i]
+        distances[i] = abs(dot(a_i_a_j, a_k)) / norm(a_i_a_j)
+    return max(distances) / 2.0
+
+
+def grid_number(lattice_vectors: np.ndarray, max_length: float):
+    a = [ceil(max_length / norm(lattice_vectors[i])) for i in range(3)]
+    return a[0] * a[1] * a[2]
 
 
 @dataclass
 class Ewald(MSONable):
     """Container class for anisotropic Ewald parameter and related properties
 
-    lattice (Lattice):
-        The given lattice.
-    dielectric_tensor (3x3 np.array):
-        Static dielectric tensor where the directions are compatible
-        with the lattice.
-    ewald_param (float):
+    ewald_param:
         A parameter used for evaluating Ewald sum.
-    accuracy (float):
+    accuracy:
         product of cutoff radius of G-vector and gaussian FWHM. Increasing this
         value, calculation will be more accurate, but slower.
     """
-    lattice: Lattice
+    lattice: np.ndarray
     dielectric_tensor: np.ndarray
     ewald_param: float
     accuracy: float
 
     @property
-    def volume(self):
-        return self.lattice.volume
+    def reciprocal_lattice(self):
+        return inv(self.lattice).T * 2 * pi
 
     @property
-    def rec_lattice_matrix(self):
-        return self.lattice.reciprocal_lattice.matrix
+    def volume(self):
+        return det(self.lattice)
+
+
+def make_ewald(structure: IStructure,
+               dielectric_tensor: np.ndarray,
+               convergence: float = 1.05,
+               accuracy: float = 25.0):
+    """Get optimized ewald parameter.
+
+    determine initial ewald parameter to satisfy following:
+    max_int(Real) = max_int(Reciprocal)
+    in neighbor_lattices function.
+
+    Left term:
+    max_int(Real) = 2 * x * Y  / l_r, where x, Y, and l_r are ewald,
+    prod_cutoff_fwhm, and axis length of real lattice, respectively.
+
+    Right term:
+    max_int(reciprocal) = Y  / (x * l_g)
+    where l_g is axis length of reciprocal lattice, respectively.
+    Then, x = sqrt(l_g / l_r / 2)
+    gmean : geometric mean,  (a1 * a2 * a3)^(1/3)
+
+    initial_ewald_param (float):
+        Initial guess of parameter.
+    convergence (float):
+        If 1/convergence < n_(real)/n_(reciprocal) < convergence,
+        where n_(real) and n_(reciprocal) is number of real lattices
+        and reciprocal lattices, finishes optimization and
+        returns ewald_param.
+    accuracy (float):
+        product of cutoff radius of G-vector and gaussian FWHM.
+        Increasing this value, calculation will be more accurate, but slower.
+    """
+    root_det_dielectric = np.sqrt(np.linalg.det(dielectric_tensor))
+    lattice = structure.lattice.matrix
+    reciprocal_lattice = structure.lattice.reciprocal_lattice.matrix
+    cube_root_vol = pow(structure.lattice.volume, 1 / 3)
+
+    l_r = mstats.gmean([norm(v) for v in lattice])
+    l_g = mstats.gmean([norm(v) for v in reciprocal_lattice])
+    ewald_param = sqrt(l_g / l_r / 2) * cube_root_vol / root_det_dielectric
+
+    for i in range(10):
+        ewald = ewald_param / cube_root_vol * root_det_dielectric
+
+        max_r_vector_norm = accuracy / ewald
+        real_num = grid_number(lattice, max_r_vector_norm)
+
+        max_g_vector_norm = 2 * ewald * accuracy
+        rec_num = grid_number(reciprocal_lattice, max_g_vector_norm)
+
+        if 1 / convergence < real_num / rec_num < convergence:
+            return Ewald(lattice, dielectric_tensor, ewald_param, accuracy)
+        else:
+            ewald_param *= (real_num / rec_num) ** (1 / 6)
+    else:
+        raise ValueError("The initial ewald param may not be adequate.")
+
 
 
--- pydefect/tests/corrections/efnv_correction/test_ewald.py
+++ pydefect/tests/corrections/efnv_correction/test_ewald.py
@@ -5,13 +5,37 @@ import numpy as np
 import pytest
 from pymatgen import Lattice
 
-from pydefect.corrections.efnv_correction.ewald import Ewald
+from pydefect.corrections.efnv_correction.efnv_correction import \
+    create_lattice_set
+from pydefect.corrections.efnv_correction.ewald import Ewald, \
+    calc_max_sphere_radius, create_lattice_set_2
 from pydefect.tests.helpers.assertion import assert_msonable
+
+
+def test_calc_max_sphere_radius():
+    lattice_vectors_1 = np.array([[5, 0, 0], [0, 10, 0], [0, 0, 20]])
+    assert calc_max_sphere_radius(lattice_vectors_1) == 10.0
+
+    lattice_vectors_2 = np.array([[10, 0, 0], [0, 10, 0], [10, 10, 10]])
+    assert calc_max_sphere_radius(lattice_vectors_2) == 5.0
+
+
+lattice_vectors_1 = np.array([[10, 10, 0], [10, -10, 0], [0, 0, 14]])
+def test_lattice_set():
+    actual = create_lattice_set(lattice_vectors_1, max_length=15)
+    expected = [[-10, -10, 0], [-10, 10, 0], [0, 0, -14], [0, 0, 0], [0, 0, 14],
+                [10, -10, 0], [10, 10, 0]]
+    assert actual == expected
+
+
+def test_lattice_speed_set():
+    actual = create_lattice_set_2(lattice_vectors_1, max_length=1500)
+    print(len(actual))
 
 
 @pytest.fixture
 def ewald():
-    return Ewald(lattice=Lattice.cubic(3.0),
+    return Ewald(lattice=Lattice.cubic(3.0).matrix,
                  dielectric_tensor=np.array([[1.0, 0.0, 0.0],
                                              [0.0, 1.0, 0.0],
                                              [0.0, 0.0, 1.0]]),
@@ -21,11 +45,14 @@ def ewald():
 
 def test_ewald(ewald):
     assert ewald.volume == 3.0 ** 3
-    assert (ewald.rec_lattice_matrix == np.eye(3) * (2 * np.pi / 3)).all()
+    assert (ewald.reciprocal_lattice == np.eye(3) * (2 * np.pi / 3)).all()
 
 
 def test_msonable(ewald):
     assert_msonable(ewald)
+
+# def test_make_ewald(ewald):
+
 
 
 """
