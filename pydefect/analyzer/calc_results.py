5d28b0f8dfcf242dbc398ff9bee126723da484ca
yuuukuma <yuuukuma@gmail.com>
2020-05-26 11:06:46 +0900

    Use ToJsonFileMixIn in CalcResults in calc_results.py

--- pydefect/analyzer/calc_results.py
+++ pydefect/analyzer/calc_results.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
 from dataclasses import dataclass
-from pathlib import Path
 from typing import List, Optional
 
 from monty.json import MSONable
-from monty.serialization import loadfn
 from pymatgen import IStructure
+
+from pydefect.mix_in import ToJsonFileMixIn
 
 
 @dataclass
-class CalcResults(MSONable):
+class CalcResults(MSONable, ToJsonFileMixIn):
     structure: IStructure
     site_symmetry: str
     energy: float
@@ -24,9 +24,5 @@ class CalcResults(MSONable):
     electronic_conv: Optional[bool] = None
     ionic_conv: Optional[bool] = None
 
-    def to_json_file(self, filename: str = "calc_results.json") -> None:
-        Path(filename).write_text(self.to_json())
 
-    @classmethod
-    def from_json_file(cls, filename: str = "calc_results.json"):
-        return loadfn(filename)
+
--- pydefect/tests/analyzer/test_calc_results.py
+++ pydefect/tests/analyzer/test_calc_results.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
-
+import pytest
 from pymatgen.io.vasp import Vasprun, Outcar
 
 from pydefect.cli.vasp.make_calc_results import make_calc_results_from_vasp
@@ -8,10 +8,17 @@ from pydefect.tests.helpers.assertion import assert_msonable, \
     assert_json_roundtrip
 
 
-def test_calc_results_msonable(vasp_files, tmpdir):
+@pytest.fixture
+def calc_results(vasp_files):
     vasprun = Vasprun(vasp_files / "MgO_conv_Va_O_0" / "vasprun.xml")
     outcar = Outcar(vasp_files / "MgO_conv_Va_O_0" / "OUTCAR")
-    calc_results = make_calc_results_from_vasp(vasprun, outcar)
+    return make_calc_results_from_vasp(vasprun, outcar)
+
+
+def test_calc_results_msonable(calc_results):
     assert_msonable(calc_results)
+
+
+def test_calc_results_json_roundtrip(calc_results, tmpdir):
     assert_json_roundtrip(calc_results, tmpdir)
 
