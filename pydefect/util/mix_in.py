d20dfbbfc21d8132ae34579a5b062801f02a6eab
yuuukuma <yuuukuma@gmail.com>
2020-05-26 12:34:27 +0900

    Refactor miscs in mix_in.py

--- pydefect/util/mix_in.py
+++ pydefect/util/mix_in.py
@@ -8,11 +8,12 @@ from typing import Optional
 class ToJsonFileMixIn:
     def to_json_file(self, filename: Optional[str] = None) -> None:
         filename = filename or self._filename
-        print(filename)
         Path(filename).write_text(self.to_json())
 
     @property
     def _filename(self):
-        """ ClassForThis -> class_for_this.json"""
+        """ ClassForThis -> class_for_this.json
+        https://stackoverflow.com/questions/7322028/how-to-replace-uppercase-with-underscore
+        """
         class_name = self.__class__.__name__
         return re.sub("(?<!^)(?=[A-Z])", "_", class_name).lower() + ".json"
