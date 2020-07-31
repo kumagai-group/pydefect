0a0560802e3be3e0ee85816c8424d5eb15b3ffed
yuuukuma <yuuukuma@gmail.com>
2020-07-31 15:44:08 +0900

    Add defect_charge_distribution.py

---
+++ pydefect/tests/analyzer/test_defect_charge_distribution.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020 Kumagai group.
+
+import numpy as np
+import pytest
+from pydefect.analyzer.defect_charge_distribution import RadialDist
+from pymatgen import Structure, Lattice, Spin
+from pymatgen.io.vasp import Chgcar
+
+
+def test():
+    structure = Structure(Lattice.cubic(3), ["H"], [[0, 0, 0]])
+    data = {"total": np.array([[[3]*3]*3]*3), "diff": np.array([[[-1]*3]*3]*3)}
+    chgcar = Chgcar(structure, data)
+
+    rad = RadialDist(chgcar, [0, 0, 0])
+    assert len(rad.distances_data) == 1 + 6 + 12
+    np.testing.assert_almost_equal(rad.distances_data[0][0], np.array([-1/3, -1./3, 0.0]))
+    assert rad.distances_data[0][1] == np.sqrt(2)
+    assert rad.distances_data[0][2] == 24
+    np.testing.assert_almost_equal(rad.distances_data[0][3], np.array([-1, -1, 0.0]))
+
+    hist_data, half_point, summed = rad.histogram(Spin.up)
+    np.testing.assert_almost_equal(hist_data[0], [5.00000000e-02, 1.16355283e-03])
+
+
+@pytest.skip
+def test_actual_files():
+    chgcar = Chgcar.from_file("PARCHG.0125.ALLK")
+    rad = RadialDist(chgcar, [0.25, 0, 0])
+    print(rad.histogram(Spin.up))
+
+
