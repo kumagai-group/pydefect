a70c0a213b4c5c9032341e00d4139024f28e2668
yuuukuma <yuuukuma@gmail.com>
2020-08-09 20:46:01 +0900

    Add make_atom_vasp_set.py from vise

---
+++ pydefect/chem_pot_diag/datasets/make_atom_vasp_set.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+from pathlib import Path
+
+import fire
+from pymatgen import Structure, Lattice
+from pymatgen.io.vasp.sets import MPRelaxSet
+from vise.input_set.datasets.potcar_set import PotcarSet
+from vise.input_set.input_options import CategorizedInputOptions
+from vise.input_set.task import Task
+from vise.input_set.vasp_input_files import VaspInputFiles
+from vise.input_set.xc import Xc
+
+
+def make_atom_vasp_set(potcar_set: PotcarSet, xc: Xc):
+    for element, potcar in potcar_set.potcar_dict().items():
+        Path(element).mkdir()
+        structure = Structure(Lattice.cubic(10),
+                              coords=[[0.5]*3], species=[element])
+        input_options = CategorizedInputOptions(structure,
+                                                task=Task.cluster_opt,
+                                                xc=xc,
+                                                potcar_set=potcar_set)
+        vasp_input_files = VaspInputFiles(
+            input_options,
+            overridden_incar_settings={"ISPIN": 2})
+        vasp_input_files.create_input_files(dirname=Path(element))
+
+
+def make_atom_mp_relax_set():
+    for element, potcar in PotcarSet.mp_relax_set.potcar_dict().items():
+        if potcar is None:
+            continue
+        Path(element).mkdir()
+        structure = Structure(Lattice.cubic(10),
+                              coords=[[0.5]*3], species=[element])
+
+        mp_set = MPRelaxSet(structure, user_incar_settings={"ISIF": 2, "ISMEAR": 0})
+        mp_set.write_input(element)
+
+
+if __name__ == '__main__':
+    def make_set(_set, xc):
+        make_atom_vasp_set(PotcarSet.from_string(_set), Xc.from_string(xc))
+
+    fire.Fire(make_set)
---
+++ pydefect/tests/chem_pot_diag/test_make_atom_vasp_set.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+
+#
+# def test(tmpdir, mocker):
+#     tmpdir.chdir()
+#     a = PotcarSet.normal
+#     make_atom_vasp_set(a, Xc.pbesol)
+#
+#
+#
+# def test_atom_mp_relax_set(tmpdir):
+#     print(tmpdir)
+#     tmpdir.chdir()
+#     make_atom_mp_relax_set()
+
+
+"""
+TODO
+- Make H dir with potcar inside
+
+DONE
+"""
