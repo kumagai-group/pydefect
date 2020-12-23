32c09231ca30295bad1a79480570affe8b879b5c
yuuukuma <yuuukuma@gmail.com>
2020-12-23 13:45:43 +0900

    Update unittests

--- pydefect/tests/input_maker/test_defect_set_maker.py
+++ pydefect/tests/input_maker/test_defect_set_maker.py
@@ -10,12 +10,12 @@ def test_defect_set_maker(supercell_info):
                 SimpleDefect(None, "He1", [0]),
                 SimpleDefect("H", "i1", [-1, 0, 1]),
                 SimpleDefect("He", "i1", [0])}
-    assert defect_maker.defect_set.defect_calc_results == expected
+    assert defect_maker.defect_set == expected
 
 
 def test_defect_set_maker_oxi_state(supercell_info):
     defect_maker = DefectSetMaker(supercell_info, {"H": 2})
-    assert defect_maker.defect_set.defect_calc_results == {SimpleDefect(None, "H1", [-2, -1, 0]),
+    assert defect_maker.defect_set == {SimpleDefect(None, "H1", [-2, -1, 0]),
                                                            SimpleDefect(None, "He1", [0]),
                                                            SimpleDefect("H", "i1", [0, 1, 2]),
                                                            SimpleDefect("He", "i1", [0])}
@@ -23,7 +23,7 @@ def test_defect_set_maker_oxi_state(supercell_info):
 
 def test_defect_set_maker_dopants(supercell_info):
     defect_maker = DefectSetMaker(supercell_info, dopants=["Li"])
-    assert set(defect_maker.defect_set.defect_calc_results) == {
+    assert set(defect_maker.defect_set) == {
         SimpleDefect(None, "H1", [-1, 0, 1]),
         SimpleDefect(None, "He1", [0]),
         SimpleDefect("Li", "H1", [0]),
@@ -36,7 +36,7 @@ def test_defect_set_maker_dopants(supercell_info):
 def test_defect_set_maker_electroneg_diff(supercell_info):
     defect_maker = DefectSetMaker(supercell_info, dopants=["Li"],
                                   ele_neg_diff=2.2 - 0.98 - 1e-5)
-    assert set(defect_maker.defect_set.defect_calc_results) == {
+    assert set(defect_maker.defect_set) == {
         SimpleDefect(None, "H1", [-1, 0, 1]),
         SimpleDefect(None, "He1", [0]),
         SimpleDefect("H", "i1", [-1, 0, 1]),
@@ -48,6 +48,6 @@ def test_defect_set_maker_electroneg_diff(supercell_info):
 def test_defect_set_maker_screening(supercell_info):
     defect_maker = DefectSetMaker(supercell_info, keywords=["Va_H1"])
     expected = {SimpleDefect(None, "H1", [-1, 0, 1])}
-    assert defect_maker.defect_set.defect_calc_results == expected
+    assert defect_maker.defect_set == expected
 
 
