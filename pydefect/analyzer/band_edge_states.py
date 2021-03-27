674a73b3e56a8e54b0d03fa6c60d5c3b4a2de4df
yuuukuma <yuuukuma@gmail.com>
2021-03-27 09:06:29 +0900

    Modify band_edge_states.py and its unittest

--- pydefect/analyzer/band_edge_states.py
+++ pydefect/analyzer/band_edge_states.py
@@ -6,8 +6,6 @@ from pathlib import Path
 from typing import List, Dict, Tuple, Optional
 
 from monty.json import MSONable
-from monty.serialization import loadfn
-from vise.util.enum import ExtendedEnum
 from vise.util.mix_in import ToJsonFileMixIn
 
 
@@ -19,68 +17,93 @@ class BandEdgeEigenvalues(MSONable, ToJsonFileMixIn):
     lowest_band_index: int
 
 
-class EdgeState(MSONable, ExtendedEnum):
-    donor_phs = "Donor PHS"
-    acceptor_phs = "Acceptor PHS"
-    in_gap_state = "In-gap state"
-    no_in_gap = "No in-gap state"
-    unknown = "Unknown"
+@dataclass
+class OrbitalInfo(MSONable):
+    """Code and its version dependent quantities. """
+    energy: float  # max eigenvalue
+    # {"Mn": [0.01, ..], "O": [0.03, 0.5]},
+    # where lists contain s, p, d, (f) orbital components.
+    orbitals: Dict[str, List[float]]
+    occupation: float
+
+
+@dataclass
+class BandEdgeOrbitalInfos(MSONable, ToJsonFileMixIn):
+    orbital_infos: List[List[List["OrbitalInfo"]]]  # [spin, k-idx, band-idx]
+    kpt_coords: List[Tuple[float, float, float]]
+    kpt_weights: List[float]
+    lowest_band_index: int
+
+
+@dataclass
+class LocalizedOrbital(MSONable):
+    band_idx: int
+    ave_energy: float
+    occupation: float
+    orbitals: Dict[str, List[float]]
+#    radius: Optional[float] = None
+#    center: Optional[Tuple[float, float, float]] = None
+
+
+@dataclass
+class EdgeInfo(MSONable):
+    band_idx: int
+    kpt_coord: Tuple[float, float, float]
+    orbital_info: "OrbitalInfo"
+
+    @property
+    def orbitals(self):
+        return self.orbital_info.orbitals
+
+    @property
+    def energy(self):
+        return self.orbital_info.energy
+
+    @property
+    def occupation(self):
+        return self.orbital_info.occupation
+
+
+@dataclass
+class PerfectBandEdgeState(MSONable, ToJsonFileMixIn):
+    vbm_info: EdgeInfo  # None for metals
+    cbm_info: EdgeInfo
+
+
+@dataclass
+class BandEdgeState(MSONable):
+    vbm_info: EdgeInfo
+    cbm_info: EdgeInfo
+    vbm_orbital_diff: float
+    cbm_orbital_diff: float
+    localized_orbitals: List[LocalizedOrbital]
 
     @property
     def is_shallow(self):
-        return self in [self.acceptor_phs, self.donor_phs]
+        return self.vbm_info.occupation < 0.5 or self.cbm_info.occupation > 0.5
 
 
 @dataclass
 class BandEdgeStates(MSONable, ToJsonFileMixIn):
-    states: List[EdgeState]  # by spin.
+    states: List[BandEdgeState]  # by spin.
 
     @property
     def is_shallow(self):
-        return any([s.is_shallow for s in self.states])
-
-    def to_yaml(self, filename="band_edge_states.yaml"):
-        d = [f"up: {self.states[0]}"]
-        if len(self.states) == 2:
-            d.append(f"down: {self.states[1]}")
-        Path(filename).write_text("\n".join(d))
-
-    @classmethod
-    def from_yaml(cls, filename="band_edge_states.yaml"):
-        d = loadfn(filename)
-        states = [EdgeState(d.pop("up"))]
-        if d:
-            states.append(EdgeState(d.pop("down")))
-        return cls(states)
-
-
-class EdgeCharacters(Sequence, MSONable, ToJsonFileMixIn):
-    def __init__(self, edge_characters: List["EdgeCharacter"]):
-        self.edge_characters = edge_characters  # [by spin]
-
-    def __getitem__(self, item):
-        return self.edge_characters[item]
-
-    def __len__(self):
-        return len(self.edge_characters)
-
-    def __str__(self):
-        strings = []
-        for spin, edge_char in zip(["up", "down"], self.edge_characters):
-            strings.append(f"""{spin:>4}: {edge_char}""")
-        return "\n".join(strings)
+        if any([i.is_shallow for i in self.states]):
+            return IsShallow(True)
+        return IsShallow(False)
 
 
 @dataclass
-class EdgeCharacter(MSONable):
-    """Code and its version dependent quantities. """
-    hob_bottom_e: float  # Highest-occupied band
-    lub_top_e: float  # Lowest-unoccupied band
-    vbm: Optional[float]  # None for metals
-    cbm: Optional[float]
-    # {"Mn": [0.01, ..], "O": [0.03, 0.5]},
-    # where lists contain s, p, d, (f) orbital components.
-    vbm_orbitals: Dict[str, List[float]]
-    cbm_orbitals: Dict[str, List[float]]
-    vbm_participation_ratio: Optional[float] = None  # participation ratio
-    cbm_participation_ratio: Optional[float] = None
+class IsShallow:
+    is_shallow: bool
+
+    def to_yaml(self, filename="is_shallow.yaml"):
+        Path(filename).write_text(str(self.is_shallow))
+
+    @classmethod
+    def from_yaml(cls, filename="band_edge_states.yaml"):
+        first_letter = Path(filename).read_text()[0].upper()
+        is_shallow = True if first_letter == "T" else False
+        return cls(is_shallow=is_shallow)
+
--- pydefect/tests/analyzer/test_band_edge_states.py
+++ pydefect/tests/analyzer/test_band_edge_states.py
@@ -4,7 +4,8 @@ from pathlib import Path
 
 import pytest
 from pydefect.analyzer.band_edge_states import BandEdgeEigenvalues, \
-    BandEdgeStates, EdgeState, EdgeCharacter, EdgeCharacters
+    BandEdgeStates, OrbitalInfo, BandEdgeOrbitalInfos, PerfectBandEdgeState, \
+    EdgeInfo, BandEdgeState, LocalizedOrbital, IsShallow
 from vise.tests.helpers.assertion import assert_msonable, assert_json_roundtrip
 
 
@@ -24,58 +25,58 @@ def test_band_edge_eigenvalues_to_json_file(band_edge_eigenvalues, tmpdir):
 
 
 @pytest.fixture
-def band_edge_states():
-    return BandEdgeStates([EdgeState.donor_phs, EdgeState.no_in_gap])
+def orbital_info():
+    return OrbitalInfo(energy=1.0, orbitals={"Mn": [0.5, 0.6]}, occupation=1.0)
 
 
-def test_band_edge_states_is_shallow(band_edge_states):
-    assert band_edge_states.is_shallow is True
-    actual = BandEdgeStates([EdgeState.in_gap_state, EdgeState.no_in_gap])
-    assert actual.is_shallow is False
-
-
-def test_band_edge_states_msonable(band_edge_states):
-    assert_msonable(band_edge_states)
-
-
-def test_band_edge_states_to_json_file(band_edge_states, tmpdir):
-    assert_json_roundtrip(band_edge_states, tmpdir)
-
-
-def test_band_edge_states_yaml(band_edge_states, tmpdir):
-    print(tmpdir)
-    tmpdir.chdir()
-    band_edge_states.to_yaml()
-    expected = """up: Donor PHS
-down: No in-gap state"""
-    assert Path("band_edge_states.yaml").read_text() == expected
-
-    actual = band_edge_states.from_yaml("band_edge_states.yaml")
-    assert actual == band_edge_states
+def test_orbital_info_msonable(orbital_info):
+    assert_msonable(orbital_info)
 
 
 @pytest.fixture
-def edge_characters():
-    edge_character = EdgeCharacter(vbm_participation_ratio=0.1,
-                                   cbm_participation_ratio=0.2,
-                                   hob_bottom_e=0.3,
-                                   lub_top_e=0.4,
-                                   vbm=0.5,
-                                   cbm=0.6,
-                                   vbm_orbitals={"Mn": [0.5, 0.6]},
-                                   cbm_orbitals={"Mn": [0.7, 0.8]})
-    return EdgeCharacters([edge_character])
+def band_edge_orbital_infos(orbital_info):
+    return BandEdgeOrbitalInfos(orbital_infos=[[[orbital_info]]],
+                                kpt_coords=[(0.0, 0.0, 0.0)],
+                                kpt_weights=[1.0],
+                                lowest_band_index=10)
 
 
-def test_edge_characters_msonable(edge_characters):
-    assert_msonable(edge_characters)
+def test_band_edge_orbital_info_json_roundtrip(band_edge_orbital_infos, tmpdir):
+    assert_json_roundtrip(band_edge_orbital_infos, tmpdir)
 
 
-def test_edge_characters_to_json_file(edge_characters, tmpdir):
-    assert_json_roundtrip(edge_characters, tmpdir)
+@pytest.fixture
+def perfect_band_edge_state(orbital_info):
+    vbm_info = EdgeInfo(band_idx=10, kpt_coord=(0.0, 0.0, 0.0),
+                        orbital_info=orbital_info)
+    return PerfectBandEdgeState(vbm_info=vbm_info, cbm_info=vbm_info)
 
 
-def test_str(edge_characters):
-    assert str(edge_characters) == """  up: EdgeCharacter(hob_bottom_e=0.3, lub_top_e=0.4, vbm=0.5, cbm=0.6, vbm_orbitals={'Mn': [0.5, 0.6]}, cbm_orbitals={'Mn': [0.7, 0.8]}, vbm_participation_ratio=0.1, cbm_participation_ratio=0.2)"""
+def test_perfect_edge_state_json_roundtrip(perfect_band_edge_state, tmpdir):
+    assert_json_roundtrip(perfect_band_edge_state, tmpdir)
 
+
+@pytest.fixture
+def band_edge_states(orbital_info):
+    vbm_info = EdgeInfo(band_idx=10, kpt_coord=(0.0, 0.0, 0.0),
+                        orbital_info=orbital_info)
+    cbm_info = EdgeInfo(band_idx=12, kpt_coord=(0.0, 0.0, 0.0),
+                        orbital_info=orbital_info)
+    localized_orbital = LocalizedOrbital(
+        band_idx=11, ave_energy=1.9, occupation=1.0,
+        orbitals={"H": [1.0, 0.0, 0.0, 0.0]})
+    band_edge_state = BandEdgeState(vbm_info=vbm_info, cbm_info=cbm_info,
+                                    vbm_orbital_diff=0.5,
+                                    cbm_orbital_diff=0.5,
+                                    localized_orbitals=[localized_orbital])
+    return BandEdgeStates(states=[band_edge_state])
+
+
+def test_band_edge_states_json_roundtrip(band_edge_states, tmpdir):
+    assert_json_roundtrip(band_edge_states, tmpdir)
+
+
+@pytest.fixture
+def is_shallow():
+    return IsShallow(is_shallow=True)
 
