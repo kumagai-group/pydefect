bcda94b366f3844802d78d39371c06388e2d8684
yuuukuma <yuuukuma@gmail.com>
2021-03-28 10:15:05 +0900

    Add make_perfect_band_edge_state.py

---
+++ pydefect/cli/vasp/make_perfect_band_edge_state.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020 Kumagai group.
+from pydefect.analyzer.band_edge_states import PerfectBandEdgeState, EdgeInfo, \
+    OrbitalInfo
+from pydefect.cli.vasp.make_band_edge_orbital_infos import \
+    calc_orbital_character
+from pydefect.defaults import defaults
+from pymatgen import Spin
+from pymatgen.io.vasp import Procar, Vasprun, Outcar
+from vise.analyzer.vasp.band_edge_properties import VaspBandEdgeProperties
+
+
+def make_perfect_band_edge_state_from_vasp(
+        procar: Procar, vasprun: Vasprun, outcar: Outcar
+) -> PerfectBandEdgeState:
+
+    band_edge_prop = VaspBandEdgeProperties(vasprun, outcar,
+                                            defaults.integer_criterion)
+    orbs, s = procar.data, vasprun.final_structure
+    vbm_info = get_edge_info(band_edge_prop.vbm_info, orbs, s, vasprun)
+    cbm_info = get_edge_info(band_edge_prop.cbm_info, orbs, s, vasprun)
+    return PerfectBandEdgeState(vbm_info, cbm_info)
+
+
+def get_edge_info(ei, orbs, s, vasprun):
+    orbitals = calc_orbital_character(
+        orbs, s, Spin.up, ei.kpoint_index, ei.band_index)
+    e, occ = vasprun.eigenvalues[Spin.up][ei.kpoint_index, ei.band_index, :]
+    orb_info = OrbitalInfo(energy=e, occupation=occ, orbitals=orbitals)
+    return EdgeInfo(ei.band_index, tuple(ei.kpoint_coords), orb_info)
+
+
---
+++ pydefect/tests/cli/vasp/test_make_perfect_band_edge_state.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020 Kumagai group.
+from pydefect.analyzer.band_edge_states import PerfectBandEdgeState, EdgeInfo, \
+    OrbitalInfo
+from pydefect.cli.vasp.make_perfect_band_edge_state import \
+    make_perfect_band_edge_state_from_vasp
+from pymatgen.io.vasp import Procar, Vasprun, Outcar
+
+
+def test_make_perfect_band_edge_state_from_vasp(vasp_files):
+    procar = Procar(vasp_files / "MgO_2x2x2_perfect" / "PROCAR")
+    vasprun = Vasprun(vasp_files / "MgO_2x2x2_perfect" / "vasprun.xml")
+    outcar = Outcar(vasp_files / "MgO_2x2x2_perfect" / "OUTCAR")
+    actual = make_perfect_band_edge_state_from_vasp(procar, vasprun, outcar)
+
+    vbm_info = EdgeInfo(
+        band_idx=127, kpt_coord=(0.25, 0.25, 0.25),
+        orbital_info=OrbitalInfo(energy=2.7746,
+                                 orbitals={'Mg': [0.0, 0.0, 0.0, 0.0],
+                                           'O': [0.0, 0.704, 0.0, 0.0]},
+                                 occupation=1.0))
+    cbm_info = EdgeInfo(
+        band_idx=128, kpt_coord=(0.25, 0.25, 0.25),
+        orbital_info=OrbitalInfo(energy=8.2034,
+                                 orbitals={'Mg': [0.192, 0.0, 0.0, 0.0],
+                                           'O': [0.224, 0.096, 0.0, 0.0]},
+                                 occupation=0.0))
+    expected = PerfectBandEdgeState(vbm_info=vbm_info, cbm_info=cbm_info)
+    assert actual == expected
