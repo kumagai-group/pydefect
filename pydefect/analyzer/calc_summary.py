2bd1116f7981803235f5cd540b5aa17534a7577c
yuuukuma <yuuukuma@gmail.com>
2021-04-13 08:00:56 +0900

    Add calc_summary.py

---
+++ pydefect/analyzer/calc_summary.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020 Kumagai group.
+from dataclasses import dataclass
+from typing import Optional, Dict
+
+from monty.json import MSONable
+from pydefect.analyzer.defect_structure_info import SymmRelation, DefectType
+from tabulate import tabulate
+
+
+@dataclass
+class SingleCalcSummary(MSONable):
+    charge: int
+    ion_io: dict
+    electronic_conv: Optional[bool] = None
+    ionic_conv: Optional[bool] = None
+    is_energy_strange: Optional[bool] = None
+    same_config_from_init: Optional[bool] = None
+    defect_type: Optional[DefectType] = None
+    symm_relation: Optional[SymmRelation] = None
+    donor_phs: Optional[bool] = None
+    acceptor_phs: Optional[bool] = None
+    unoccupied_deep_state: Optional[bool] = None
+    occupied_deep_state: Optional[bool] = None
+    same_structure: Optional[str] = None
+
+    def same_ion_charge_io(self, other: "SingleCalcSummary"):
+        return self.charge == other.charge and self.ion_io == other.ion_io
+
+    @property
+    def is_converged(self):
+        return self.electronic_conv and self.ionic_conv
+
+    @property
+    def is_proper_result(self):
+        return self.is_converged is True and self.is_energy_strange is False
+
+    @property
+    def is_unusual(self):
+        return (self.symm_relation is SymmRelation.supergroup or
+                self.same_config_from_init is False)
+
+    @property
+    def config_list(self):
+        if self.is_proper_result:
+            result = [".", ".", "."]
+            if self.same_config_from_init in (False, None):
+                result.extend([str(self.same_config_from_init),
+                               str(self.defect_type),
+                               str(self.symm_relation)])
+            else:
+                result.extend([".", ".", "."])
+            return result
+        elif self.is_converged:
+            return [".", ".", self.is_energy_strange]
+        else:
+            return [self.electronic_conv, self.ionic_conv]
+
+
+@dataclass
+class CalcSummary(MSONable):
+    single_summaries: Dict[str, SingleCalcSummary]
+
+    def __str__(self):
+        lines = [["name", "Ele. conv.", "Ionic conv.", "Is energy strange",
+                  "Same config.", "Defect type", "Symm. Relation"]]
+        for k, v in self.single_summaries.items():
+            lines.append([k] + v.config_list)
+        return tabulate(lines, stralign="center", tablefmt="pipe")
---
+++ pydefect/tests/analyzer/test_calc_summary.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020 Kumagai group.
+from copy import copy
+
+import pytest
+from pydefect.analyzer.calc_summary import SingleCalcSummary, CalcSummary
+from pydefect.analyzer.defect_structure_info import DefectType, SymmRelation
+from vise.tests.helpers.assertion import assert_msonable
+
+
+@pytest.fixture
+def single_summary():
+    return SingleCalcSummary(charge=0,
+                             ion_io={"O": -1},
+                             electronic_conv=True,
+                             ionic_conv=True,
+                             is_energy_strange=False,
+                             same_config_from_init=True,
+                             defect_type=DefectType.vacancy,
+                             symm_relation=SymmRelation.same,
+                             donor_phs=False,
+                             acceptor_phs=False,
+                             unoccupied_deep_state=True,
+                             occupied_deep_state=True)
+
+
+def test_single_calc_summary_msonable(single_summary):
+    assert_msonable(single_summary)
+
+
+def test_single_calc_summary_properties(single_summary):
+    other = copy(single_summary)
+    assert single_summary.same_ion_charge_io(other) is True
+    other.charge = 1
+    assert single_summary.same_ion_charge_io(other) is False
+
+    assert single_summary.is_converged is True
+    assert single_summary.is_proper_result is True
+    assert single_summary.is_unusual is False
+
+
+@pytest.fixture
+def calc_summary(single_summary):
+    second = SingleCalcSummary(charge=1,
+                               ion_io={"Mg": -1},
+                               electronic_conv=True,
+                               ionic_conv=False,
+                               is_energy_strange=False,
+                               same_config_from_init=False,
+                               defect_type=DefectType.vacancy_split,
+                               symm_relation=SymmRelation.supergroup,
+                               donor_phs=True,
+                               acceptor_phs=False,
+                               unoccupied_deep_state=True,
+                               occupied_deep_state=True)
+
+    return CalcSummary({"Va_O1_0": single_summary, "Va_Mg1_1": second})
+
+
+def test_calc_summary_msonable(calc_summary):
+    assert_msonable(calc_summary)
+
+
+def test_calc_summary_str(calc_summary):
+    print(calc_summary)
