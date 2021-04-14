c53e6611536b7fc7d126e74ce14ca61dd515c56c
yuuukuma <yuuukuma@gmail.com>
2021-04-14 13:12:57 +0900

    Modify make_energy according to change of EnergySummary

--- pydefect/analyzer/make_energy.py
+++ pydefect/analyzer/make_energy.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020 Kumagai group.
+from typing import Dict
+
 from pydefect.analyzer.calc_results import CalcResults
 from pydefect.analyzer.defect_energy import num_atom_differences
-from pydefect.analyzer.energy import Energy
+from pydefect.analyzer.energy import EnergySummary, Energy
+from pydefect.corrections.abstract_correction import Correction
 from pydefect.input_maker.defect_entry import DefectEntry
+from pymatgen import Element
 
 
 def make_energy(defect_entry: DefectEntry,
                 calc_results: CalcResults,
-                perfect_calc_results: CalcResults) -> Energy:
+                correction: Correction,
+                perfect_calc_results: CalcResults,
+                standard_energies: Dict[Element, float]) -> EnergySummary:
     atom_io = num_atom_differences(calc_results.structure,
-                                   perfect_calc_results.structure, True)
+                                   perfect_calc_results.structure)
+    formation_energy = calc_results.energy - perfect_calc_results.energy
+    for k, v in atom_io.items():
+        formation_energy -= standard_energies[k] * v
+    energy = Energy(formation_energy=formation_energy,
+                    atom_io=atom_io,
+                    correction_energy=correction.correction_dict)
 
-    return Energy(name=defect_entry.name,
-                  charge=defect_entry.charge,
-                  rel_energy=calc_results.energy - perfect_calc_results.energy,
-                  atom_io=atom_io)
+    return EnergySummary(defect_entry.name, defect_entry.charge, energy)
--- pydefect/tests/analyzer/test_make_energy.py
+++ pydefect/tests/analyzer/test_make_energy.py
@@ -2,10 +2,11 @@
 #  Copyright (c) 2020 Kumagai group.
 
 from pydefect.analyzer.calc_results import CalcResults
-from pydefect.analyzer.energy import Energy
+from pydefect.analyzer.energy import EnergySummary, Energy
 from pydefect.analyzer.make_energy import make_energy
+from pydefect.corrections.abstract_correction import Correction
 from pydefect.input_maker.defect_entry import DefectEntry
-from pymatgen import IStructure, Lattice
+from pymatgen import IStructure, Lattice, Element
 
 
 def test_make_energy(mocker):
@@ -18,12 +19,20 @@ def test_make_energy(mocker):
     calc_results.energy = 10.0
     calc_results.electronic_conv = False
 
+    correction = mocker.Mock(Correction, autospec=True)
+    correction.correction_dict = {"a": 10.0}
+
     p_calc_results = mocker.Mock(CalcResults, autospec=True)
     p_calc_results.structure = IStructure(Lattice.cubic(1.0),
                                           ["Mg", "O"], [[0.0]*3]*2)
     p_calc_results.energy = 1.0
 
-    actual = make_energy(defect_entry, calc_results, p_calc_results)
-    expected = Energy(name="Va_Mg1", charge=-1, rel_energy=10.0 - 1.0,
-                      atom_io={"Mg": -1})
+    standard_energies = {Element.Mg: 10.0, Element.O: 20.0}
+
+    actual = make_energy(defect_entry, calc_results, correction, p_calc_results,
+                         standard_energies)
+    expected = EnergySummary(name="Va_Mg1", charge=-1,
+                             energy=Energy(formation_energy=10.0 - 1.0 + 10,
+                                           atom_io={Element.Mg: -1},
+                                           correction_energy={"a": 10.0}))
     assert actual == expected
