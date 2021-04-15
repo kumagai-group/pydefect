f4e1eaa1f58f504b5dd1a8beaa36d637d3658977
yuuukuma <yuuukuma@gmail.com>
2021-04-15 09:12:32 +0900

    Modify class names in energy.py

--- pydefect/analyzer/energy.py
+++ pydefect/analyzer/energy.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020 Kumagai group.
 from dataclasses import dataclass
-from pathlib import Path
 from typing import Dict, Optional
 
 from monty.json import MSONable
@@ -11,7 +10,7 @@ from vise.util.mix_in import ToYamlFileMixIn
 
 
 @dataclass
-class Energy(MSONable):
+class DefectEnergy(MSONable):
     formation_energy: float
     atom_io: Dict[Element, int]
     correction_energy: Dict[str, float]
@@ -21,16 +20,12 @@ class Energy(MSONable):
     def total_correction(self) -> float:
         return sum([v for v in self.correction_energy.values()])
 
-    # def formation_energy_wo_corr(self,
-    #                              abs_chem_pot: Dict[Element, float]) -> float:
-    #     return self.rel_energy - reservoir_energy(self.atom_io, abs_chem_pot)
-
 
 @dataclass
-class EnergySummary(MSONable, ToYamlFileMixIn):
+class DefectEnergyInfo(MSONable, ToYamlFileMixIn):
     name: str
     charge: int
-    energy: Energy
+    energy: DefectEnergy
 
     def to_yaml(self) -> str:
         lines = [f"name: {self.name}",
@@ -42,16 +37,17 @@ class EnergySummary(MSONable, ToYamlFileMixIn):
         lines.append(f"correction_energy:")
         for k, v in self.energy.correction_energy.items():
             lines.append(f"  {k}: {v}")
-        is_shallow = "" if self.energy.is_shallow is None else self.energy.is_shallow
+        is_shallow = "" if self.energy.is_shallow is None \
+            else self.energy.is_shallow
         lines.append(f"is_shallow: {is_shallow}")
         return "\n".join(lines)
 
     @classmethod
-    def from_yaml(cls, filename: str = "energy.yaml") -> "EnergySummary":
+    def from_yaml(cls, filename: str = "energy.yaml") -> "DefectEnergyInfo":
         d = loadfn(filename)
         d["atom_io"] = {Element(k): v for k, v in d["atom_io"].items()} \
             if d["atom_io"] else {}
-        return cls(name=d.pop("name"), charge=d.pop("charge"), energy=Energy(**d))
+        return cls(d.pop("name"), d.pop("charge"), DefectEnergy(**d))
 
 
 def reservoir_energy(atom_io: Dict[Element, int],
--- pydefect/tests/analyzer/test_energy.py
+++ pydefect/tests/analyzer/test_energy.py
@@ -2,44 +2,34 @@
 #  Copyright (c) 2020 Kumagai group.
 
 import pytest
-from pydefect.analyzer.energy import EnergySummary, reservoir_energy, Energy
+from pydefect.analyzer.energy import DefectEnergyInfo, reservoir_energy, \
+    DefectEnergy
 from pymatgen import Element
 from vise.tests.helpers.assertion import assert_yaml_roundtrip
 
 
 @pytest.fixture
 def energy():
-    return Energy(formation_energy=1.0,
-                  atom_io={Element.O: -1},
-                  correction_energy={"1st order": 1.0, "alignment-like": 2.0},
-                  is_shallow=False)
+    return DefectEnergy(formation_energy=1.0,
+                        atom_io={Element.O: -1},
+                        correction_energy={"1st order": 1.0, "alignment": 2.0},
+                        is_shallow=False)
 
 
 def test_energy_total_correction(energy):
     assert energy.total_correction == 3.0
 
 
-# def test_defect_formation_energy_wo_corr(energy, tmpdir):
-#     actual = energy.formation_energy_wo_corr(abs_chem_pot={Element.O: 10.0})
-#     assert actual == 1.0 - 10.0
-
-
-# def test_defect_formation_energy_wo_correction(energy2, tmpdir):
-#     energy2.correction_energy = {"a": 1.0}
-#     actual = energy2.formation_energy_wo_corr(abs_chem_pot={})
-#     assert actual == 0.0
-
-
 @pytest.fixture
 def energy_summary(energy):
-    return EnergySummary(name="Va_O1", charge=1, energy=energy)
+    return DefectEnergyInfo(name="Va_O1", charge=1, energy=energy)
 
 
 @pytest.fixture
 def energy_summary2():
-    energy = Energy(formation_energy=0.0, atom_io={},
-                    correction_energy={"no correction": 0.0})
-    return EnergySummary(name="hole polaron", charge=1, energy=energy)
+    energy = DefectEnergy(formation_energy=0.0, atom_io={},
+                          correction_energy={"no correction": 0.0})
+    return DefectEnergyInfo(name="hole polaron", charge=1, energy=energy)
 
 
 def test_energy_yaml(energy_summary, tmpdir):
@@ -50,7 +40,7 @@ atom_io:
   O: -1
 correction_energy:
   1st order: 1.0
-  alignment-like: 2.0
+  alignment: 2.0
 is_shallow: False"""
     assert_yaml_roundtrip(energy_summary, tmpdir, expected_text)
 
