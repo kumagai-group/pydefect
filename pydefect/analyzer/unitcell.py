0c86fdff14e41176c5f85a04a8cbd45f441d82e1
yuuukuma <yuuukuma@gmail.com>
2021-04-18 12:29:21 +0900

    Modify Unitcell to use yaml and add system

--- pydefect/analyzer/unitcell.py
+++ pydefect/analyzer/unitcell.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
-from dataclasses import dataclass
+from collections import OrderedDict
+from dataclasses import dataclass, asdict
 from pathlib import Path
 from typing import List
 
 import numpy as np
-from monty.json import MSONable
 from monty.serialization import loadfn
-from vise.util.mix_in import ToJsonFileMixIn
+from ruamel.yaml import add_constructor, resolver, YAML
+
+
+# courtesy of https://qiita.com/konomochi/items/f5f53ba8efa07ec5089b
+add_constructor(resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+                lambda loader, node: OrderedDict(loader.construct_pairs(node)))
+
+yaml = YAML()
+yaml.default_flow_style = False
 
 
 @dataclass
-class Unitcell(MSONable, ToJsonFileMixIn):
+class Unitcell:
+    system: str
     vbm: float
     cbm: float
     ele_dielectric_const: List[List[float]]
@@ -23,18 +32,9 @@ class Unitcell(MSONable, ToJsonFileMixIn):
                  + np.array(self.ion_dielectric_const))
         return total.tolist()
 
-    def to_yaml(self, filename: str = "unitcell.yaml") -> None:
-        Path(filename).write_text(f"""vbm: {self.vbm}
-cbm: {self.cbm}
-ele_dielectric_const:
-- {self.ele_dielectric_const[0]}
-- {self.ele_dielectric_const[1]}
-- {self.ele_dielectric_const[2]}
-ion_dielectric_const:
-- {self.ion_dielectric_const[0]}
-- {self.ion_dielectric_const[1]}
-- {self.ion_dielectric_const[2]}
-""")
+    def to_yaml_file(self, filename):
+        with open(Path(filename), 'w', encoding='utf-8') as f:
+            yaml.dump(asdict(self), f)
 
     @classmethod
     def from_yaml(cls, filename: str = "unitcell.yaml"):
--- pydefect/tests/analyzer/test_unitcell.py
+++ pydefect/tests/analyzer/test_unitcell.py
@@ -14,7 +14,7 @@ from vise.tests.helpers.assertion import assert_json_roundtrip, \
 def unitcell():
     ele = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
     ion = [[10.0, 20.0, 30.0], [40.0, 50.0, 60.0], [70.0, 80.0, 90.0]]
-    return Unitcell(vbm=0.1, cbm=5.1,
+    return Unitcell(system="MgAl2O4", vbm=0.1, cbm=5.1,
                     ele_dielectric_const=ele, ion_dielectric_const=ion)
 
 
@@ -23,20 +23,29 @@ def test_unitcell(unitcell):
     np.testing.assert_array_almost_equal(unitcell.dielectric_constant, expected)
 
 
-def test_json_round_trip(unitcell, tmpdir):
-    assert_json_roundtrip(unitcell, tmpdir)
-
-
 def test_yaml_roundtrip(unitcell, tmpdir):
-    expected_text = """vbm: 0.1
+    expected_text = """system: MgAl2O4
+vbm: 0.1
 cbm: 5.1
 ele_dielectric_const:
-- [1.0, 2.0, 3.0]
-- [4.0, 5.0, 6.0]
-- [7.0, 8.0, 9.0]
+- - 1.0
+  - 2.0
+  - 3.0
+- - 4.0
+  - 5.0
+  - 6.0
+- - 7.0
+  - 8.0
+  - 9.0
 ion_dielectric_const:
-- [10.0, 20.0, 30.0]
-- [40.0, 50.0, 60.0]
-- [70.0, 80.0, 90.0]
+- - 10.0
+  - 20.0
+  - 30.0
+- - 40.0
+  - 50.0
+  - 60.0
+- - 70.0
+  - 80.0
+  - 90.0
 """
     assert_yaml_roundtrip(unitcell, tmpdir, expected_text)
