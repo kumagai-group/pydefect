d8b3f3babb793293e2e117fdbdcbfabfede936bb
yuuukuma <yuuukuma@gmail.com>
2021-04-22 20:46:04 +0900

    Move make_composition_energies_from_mp.py from make_chem_pot_diag.py

---
+++ pydefect/cli/vasp/make_composition_energies_from_mp.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020 Kumagai group.
+from itertools import groupby
+from pathlib import Path
+from typing import List, Optional, Dict
+
+from monty.serialization import loadfn
+from pydefect.chem_pot_diag.chem_pot_diag import CompositionEnergy, \
+    CompositionEnergies
+from pydefect.util.mp_tools import MpQuery
+from pymatgen.core import Composition
+from vise.atom_energies.atom_energy import mp_energies
+from vise.util.logger import get_logger
+
+parent = Path(__file__).parent
+logger = get_logger(__name__)
+
+
+def make_composition_energies_from_mp(elements: List[str],
+                                      atom_energy_yaml: Optional[str] = None,
+                                      ) ->CompositionEnergies:
+    """Obtain the energies from Materials Project.
+
+    When the atom_energy_yaml is provided, the total energies are aligned
+    via atom energies.
+    """
+    properties = ["task_id", "full_formula", "final_energy"]
+    query = MpQuery(elements, properties=properties)
+    comp_es = {}
+    if atom_energy_yaml:
+        energies = loadfn(atom_energy_yaml)
+        diff = {e: energies[e] - mp_energies[e] for e in elements}
+    else:
+        diff = {e: 0.0 for e in elements}
+
+    for m in query.materials:
+        energy = m["final_energy"]
+        for k, v in Composition(m["full_formula"]).as_dict().items():
+            energy += diff[k] * v
+        comp_es[m["full_formula"]] = CompositionEnergy(energy, m["task_id"])
+    comp_es = remove_higher_energy_comp(comp_es)
+    return CompositionEnergies(comp_es)
+
+
+def remove_higher_energy_comp(comp_energies: Dict[str, CompositionEnergy]):
+    _l = [[k, v] for k, v in comp_energies.items()]
+    result = {}
+    for _, grouped_k_v in groupby(
+            _l, key=lambda x: Composition(x[0]).reduced_formula):
+        formula, comp_e = min(list(grouped_k_v),
+                              key=lambda y: (y[1].energy
+                                             / Composition(y[0]).num_atoms))
+        result[formula] = comp_e
+    return result
+
+
---
+++ pydefect/tests/cli/vasp/test_make_composition_energies_from_mp.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020 Kumagai group.
+
+import pytest
+from pydefect.chem_pot_diag.chem_pot_diag import CompositionEnergy, \
+    CompositionEnergies
+from pydefect.cli.vasp.make_composition_energies_from_mp import \
+    make_composition_energies_from_mp, remove_higher_energy_comp
+from pymatgen.core import Composition, Element
+
+
+@pytest.fixture
+def mp_query(mocker):
+    mock = mocker.patch("pydefect.cli.vasp.make_composition_energies_from_mp.MpQuery")
+    mock.return_value.materials = \
+        [{"full_formula": "O8", "final_energy": -39.58364375, "task_id": "mp-1"},
+         {"full_formula": "Mg3", "final_energy": -4.79068775, "task_id": "mp-2"},
+         {"full_formula": "Mg1O1", "final_energy": -11.96742144, "task_id": "mp-3"}]
+    return mock
+
+
+@pytest.fixture
+def comp_energies():
+    return CompositionEnergies({
+        'O8': CompositionEnergy(-39.58364375, "mp-1"),
+        'Mg3': CompositionEnergy(-4.79068775, "mp-2"),
+        'Mg1O1': CompositionEnergy(-11.96742144, "mp-3")})
+
+# mp
+# O:  -1.54794890
+# Mg: -0.01731593
+diff = {"Mg": -0.00912097 - -0.01731593, "O": -1.61154565 - -1.54794890}
+
+
+@pytest.fixture
+def comp_energies_corr():
+    return CompositionEnergies({
+        'O8': CompositionEnergy(-39.58364375 + diff["O"] * 8, "mp-1"),
+        'Mg3': CompositionEnergy(-4.79068775 + diff["Mg"] * 3, "mp-2"),
+        'Mg1O1': CompositionEnergy(-11.96742144 + diff["Mg"] + diff["O"], "mp-3")})
+
+
+def test_make_chem_pot_diag_from_mp(mp_query, comp_energies):
+    actual = make_composition_energies_from_mp(elements=["Mg", "O"])
+    mp_query.assert_called_once_with(
+        ["Mg", "O"], properties=["task_id", "full_formula", "final_energy"])
+    assert actual == comp_energies
+
+
+def test_make_chem_pot_diag_from_mp_yaml(mp_query, tmpdir, comp_energies_corr):
+    tmpdir.chdir()
+    tmpdir.join("tmp.yaml").write("""O:  -1.61154565
+Mg: -0.00912097""")
+    actual = make_composition_energies_from_mp(elements=["Mg", "O"],
+                                               atom_energy_yaml="tmp.yaml")
+    assert actual == comp_energies_corr
+
+
+def test_remove_higher_energy_comp():
+    comp_es = CompositionEnergies({'Mg1': CompositionEnergy(-1.9, "mp-2"),
+                                   'Mg2': CompositionEnergy(-4.0, "mp-3"),
+                                   'O2': CompositionEnergy(-4.0, "mp-4"),
+                                   })
+    actual = remove_higher_energy_comp(comp_es)
+    expected = CompositionEnergies({'Mg2': CompositionEnergy(-4.0, "mp-3"),
+                                    'O2': CompositionEnergy(-4.0, "mp-4"),})
+
+    assert actual == expected
+
