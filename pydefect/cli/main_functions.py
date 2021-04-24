ef4e18c2edc06bd9738c512dea9b098e6805e672
yuuukuma <yuuukuma@gmail.com>
2021-04-24 15:51:15 +0900

    Add main_functions.py and its unittests

--- pydefect/cli/main_functions.py
+++ pydefect/cli/main_functions.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020 Kumagai group.
+from monty.serialization import loadfn
+from pydefect.analyzer.defect_energy import DefectEnergyInfo, \
+    DefectEnergySummary
+from pydefect.analyzer.defect_energy_plotter import DefectEnergyMplPlotter
+from pydefect.analyzer.make_band_edge_states import make_band_edge_states
+from pydefect.analyzer.make_defect_energy_info import make_defect_energy_info
+from pydefect.analyzer.make_defect_energy_summary import \
+    make_defect_energy_summary
 from pydefect.chem_pot_diag.chem_pot_diag import CompositionEnergies, \
-    RelativeEnergies, ChemPotDiagMaker, ChemPotDiag
+    RelativeEnergies, ChemPotDiagMaker, ChemPotDiag, StandardEnergies, \
+    TargetVertices
 from pydefect.chem_pot_diag.cpd_plotter import ChemPotDiag2DPlotlyPlotter, \
     ChemPotDiag3DPlotlyPlotter, ChemPotDiag2DMplPlotter, ChemPotDiag3DMplPlotter
+from pydefect.cli.main_tools import sanitize_matrix
+from pydefect.cli.vasp.make_efnv_correction import make_efnv_correction
+from pydefect.corrections.site_potential_plotter import SitePotentialMplPlotter
+from pydefect.input_maker.add_interstitial import append_interstitial
+from pydefect.input_maker.defect_set_maker import DefectSetMaker
+from pydefect.input_maker.supercell_maker import SupercellMaker
 from pymatgen import Composition
 from vise.util.logger import get_logger
 
@@ -11,7 +26,7 @@ from vise.util.logger import get_logger
 logger = get_logger(__name__)
 
 
-def make_std_and_rel_energies(args):
+def make_standard_and_relative_energies(args):
     comp_energies = CompositionEnergies.from_yaml(
         args.composition_energies_yaml)
     std_energies, rel_energies = comp_energies.std_rel_energies
@@ -47,3 +62,118 @@ def plot_chem_pot_diag(args):
     plt.savefig(fname="cpd.pdf")
     plt.show()
 
+
+def make_supercell(args):
+    if args.matrix:
+        matrix = sanitize_matrix(args.matrix)
+        maker = SupercellMaker(args.unitcell, matrix)
+    else:
+        kwargs = {}
+        if args.min_num_atoms:
+            kwargs["min_num_atoms"] = args.min_num_atoms
+        if args.max_num_atoms:
+            kwargs["max_num_atoms"] = args.max_num_atoms
+        maker = SupercellMaker(args.unitcell, **kwargs)
+
+    maker.supercell.structure.to(filename="SPOSCAR")
+    maker.supercell_info.to_json_file()
+
+
+def append_interstitial_to_supercell_info(args):
+    supercell_info = append_interstitial(args.supercell_info,
+                                         args.base_structure,
+                                         args.frac_coords)
+    supercell_info.to_json_file()
+
+
+def pop_interstitial_from_supercell_info(args):
+    supercell_info = args.supercell_info
+    assert args.index > 0
+    supercell_info.interstitials.pop(args.index - 1)
+    supercell_info.to_json_file()
+
+
+def make_defect_set(args):
+    supercell_info = loadfn("supercell_info.json")
+    if args.oxi_states:
+        oxi_states = dict(zip(args.oxi_states[::2], args.oxi_states[1::2]))
+    else:
+        oxi_states = None
+    maker = DefectSetMaker(supercell_info,
+                           oxi_states,
+                           args.dopants,
+                           keywords=args.kwargs)
+    maker.defect_set.to_yaml()
+
+
+def make_efnv_correction_main_func(args):
+    for d in args.dirs:
+        logger.info(f"Parsing data in {d} ...")
+        defect_entry = loadfn(d / "defect_entry.json")
+        calc_results = loadfn(d / "calc_results.json")
+        efnv = make_efnv_correction(defect_entry.charge,
+                                    calc_results,
+                                    args.perfect_calc_results,
+                                    args.unitcell.dielectric_constant)
+        efnv.to_json_file(d / "correction.json")
+
+        title = defect_entry.full_name
+        plotter = SitePotentialMplPlotter.from_efnv_corr(
+            title=title, efnv_correction=efnv)
+        plotter.construct_plot()
+        plotter.plt.savefig(fname=d / "correction.pdf")
+        plotter.plt.clf()
+
+
+def make_band_edge_states_main_func(args):
+    for d in args.dirs:
+        try:
+            print(f"-- {d}")
+            orb_infos = loadfn(d / "band_edge_orbital_infos.json")
+            band_edge_states = make_band_edge_states(orb_infos, args.p_state)
+            band_edge_states.to_json_file(d / "band_edge_states.json")
+        except ValueError as e:
+            print(e)
+
+
+def make_defect_energy_info_main_func(args):
+    for d in args.dirs:
+        try:
+            print(f"-- {d}")
+            defect_entry = loadfn(d / "defect_entry.json")
+            calc_results = loadfn(d / "calc_results.json")
+            correction = loadfn(d / "correction.json")
+            try:
+                band_edge_states = loadfn(d / "band_edge_states.json")
+            except:
+                band_edge_states = None
+            defect_energy_info = make_defect_energy_info(defect_entry,
+                                                         calc_results,
+                                                         correction,
+                                                         args.perf_calc_results,
+                                                         args.std_energies,
+                                                         band_edge_states)
+            defect_energy_info.to_yaml_file(d / "defect_energy_info.yaml")
+        except ValueError as e:
+            print(e)
+
+
+def make_defect_energy_summary_main_func(args):
+    energy_infos = [DefectEnergyInfo.from_yaml(d / "calc_results.json")
+                    for d in args.dirs]
+    target_vertices = TargetVertices.from_yaml(args.cpd_yaml)
+    defect_energy_summary = make_defect_energy_summary(
+        energy_infos, target_vertices, args.unitell, args.perfect_calc_results)
+    defect_energy_summary.to_json_file()
+
+
+def plot_defect_energy(args):
+    plotter = DefectEnergyMplPlotter(
+        defect_energy_summary=args.defect_energy_summary,
+        chem_pot_label=args.label,
+        allow_shallow=args.allow_shallow,
+        with_correction=args.with_correction,
+        label_line=args.label_line,
+        add_charges=args.add_charges)
+    plotter.construct_plot()
+    plotter.plt.savefig(f"energy_{args.label}.pdf")
--- pydefect/tests/cli/test_main_functions.py
+++ pydefect/tests/cli/test_main_functions.py
@@ -3,23 +3,36 @@
 from argparse import Namespace
 from pathlib import Path
 
+import pytest
+from monty.serialization import loadfn
+from pydefect.analyzer.band_edge_states import PerfectBandEdgeState, \
+    BandEdgeOrbitalInfos
+from pydefect.analyzer.calc_results import CalcResults
+from pydefect.analyzer.unitcell import Unitcell
 from pydefect.chem_pot_diag.chem_pot_diag import CompositionEnergies, \
     CompositionEnergy, RelativeEnergies, ChemPotDiag
-from pydefect.cli.main_functions import make_std_and_rel_energies, \
-    make_cpd_and_vertices, plot_chem_pot_diag
+from pydefect.cli.main_functions import make_standard_and_relative_energies, \
+    make_cpd_and_vertices, plot_chem_pot_diag, make_supercell, \
+    append_interstitial_to_supercell_info, \
+    pop_interstitial_from_supercell_info, make_defect_set, \
+    make_band_edge_states_main_func, make_efnv_correction_main_func
+from pydefect.corrections.efnv_correction import ExtendedFnvCorrection
+from pydefect.input_maker.defect import SimpleDefect
+from pydefect.input_maker.defect_entry import DefectEntry
+from pydefect.input_maker.defect_set import DefectSet
+from pymatgen import IStructure
 from pymatgen.core import Composition
 
 
-def test_make_std_and_rel_energies(mocker, tmpdir):
+def test_make_standard_and_relative_energies(mocker, tmpdir):
     tmpdir.chdir()
-    print(tmpdir)
     args = Namespace(composition_energies_yaml="a.yaml")
     mock = mocker.patch("pydefect.cli.main_functions.CompositionEnergies")
     mock.from_yaml.return_value = CompositionEnergies(
         {Composition("H"): CompositionEnergy(-1.0),
          Composition("He"): CompositionEnergy(-2.0),
          Composition("HHe"): CompositionEnergy(-5.0)})
-    make_std_and_rel_energies(args)
+    make_standard_and_relative_energies(args)
     mock.from_yaml.assert_called_once_with("a.yaml")
     actual = Path("standard_energies.yaml").read_text()
     expected = """H: -1.0
@@ -35,7 +48,6 @@ He: -2.0
 
 def test_make_cpd_and_vertices(mocker, tmpdir):
     tmpdir.chdir()
-    print(tmpdir)
     mock = mocker.patch("pydefect.cli.main_functions.RelativeEnergies")
     mock.from_yaml.return_value = RelativeEnergies({"HHe": -1.0})
 
@@ -64,8 +76,143 @@ B:
 
 def test_plot_chem_pot_diag(tmpdir):
     tmpdir.chdir()
-    args = Namespace(chem_pot_diag=ChemPotDiag(vertex_elements=["Mg", "O"],
-                                               polygons={"Mg": [[0.0, -3.0], [0.0, -1.0]],
-                                                         "O": [[-3.0, 0.0], [-1.0, 0.0]],
-                                                         "MgO": [[-1.0, 0.0], [0.0, -1.0]]}))
+    chem_pot_diag = ChemPotDiag(vertex_elements=["Mg", "O"],
+                                polygons={"Mg": [[0.0, -3.0], [0.0, -1.0]],
+                                          "O": [[-3.0, 0.0], [-1.0, 0.0]],
+                                          "MgO": [[-1.0, 0.0], [0.0, -1.0]]})
+    args = Namespace(chem_pot_diag=chem_pot_diag)
     plot_chem_pot_diag(args)
+
+
+def test_make_supercell_from_matrix(simple_cubic, simple_cubic_2x1x1, tmpdir):
+    tmpdir.chdir()
+    matrix = [2, 1, 1]
+    args = Namespace(unitcell=simple_cubic, matrix=matrix,
+                     min_num_atoms=None, max_num_atoms=None)
+    make_supercell(args)
+    info = loadfn("supercell_info.json")
+    assert IStructure.from_file("SPOSCAR") == simple_cubic_2x1x1
+    assert info.structure == simple_cubic_2x1x1
+    assert info.transformation_matrix == [[2, 0, 0], [0, 1, 0], [0, 0, 1]]
+
+
+def test_make_recommended_supercell(simple_cubic, simple_cubic_2x2x2, tmpdir):
+    tmpdir.chdir()
+    args = Namespace(unitcell=simple_cubic, matrix=None,
+                     min_num_atoms=8, max_num_atoms=8)
+    make_supercell(args)
+    info = loadfn("supercell_info.json")
+    assert IStructure.from_file("SPOSCAR") == simple_cubic_2x2x2
+    assert info.structure == simple_cubic_2x2x2
+    assert info.transformation_matrix == [[2, 0, 0], [0, 2, 0], [0, 0, 2]]
+
+
+def test_add_interstitials(mocker):
+    mock_1 = mocker.Mock()
+    mock_2 = mocker.Mock()
+    mock_3 = mocker.Mock()
+    args = Namespace(supercell_info=mock_1, base_structure=mock_2,
+                     frac_coords=mock_3)
+    mock = mocker.patch("pydefect.cli.main_functions.append_interstitial")
+
+    append_interstitial_to_supercell_info(args)
+    mock.assert_called_once_with(mock_1, mock_2, mock_3)
+    mock.return_value.to_json_file.assert_called_once_with()
+
+
+def test_pop_interstitials(mocker):
+    mock_si = mocker.MagicMock()
+    args = Namespace(supercell_info=mock_si, index=1000)
+
+    pop_interstitial_from_supercell_info(args)
+    assert isinstance(mock_si.interstitials, object)
+    mock_si.interstitials.pop.assert_called_once_with(999)
+    mock_si.to_json_file.assert_called_once_with()
+
+
+@pytest.mark.parametrize("oxi_states,he_vacancy_charge",
+                         ([None, [0]], [["He", 1, "Li", 1], [-1, 0, 1]]))
+def test_make_defect_set(oxi_states, he_vacancy_charge, tmpdir, supercell_info):
+    tmpdir.chdir()
+    supercell_info.to_json_file()
+    args = Namespace(oxi_states=oxi_states, dopants=["Li"],
+                     kwargs=["Li_H1", "Va_He1", "Va_H1_-1"])
+    make_defect_set(args)
+
+    simple_defects = {SimpleDefect(None, "He1", he_vacancy_charge),
+                      SimpleDefect(None, "H1", [-1]),
+                      SimpleDefect("Li", "H1", [0])}
+
+    DefectSet(defects=simple_defects).to_yaml("expected.yaml")
+    actual = Path("defect_in.yaml").read_text()
+    expected = Path("expected.yaml").read_text()
+    assert actual == expected
+
+
+def test_make_efnv_correction_from_vasp(tmpdir, mocker):
+    mock_defect_entry = mocker.Mock(spec=DefectEntry, autospec=True)
+    mock_calc_results = mocker.Mock(spec=CalcResults, autospec=True)
+
+    def side_effect(key):
+        if str(key) == "Va_O1_2/defect_entry.json":
+            mock_defect_entry.full_name = "Va_O1_2"
+            mock_defect_entry.charge = 2
+            return mock_defect_entry
+        elif str(key) == "Va_O1_2/calc_results.json":
+            return mock_calc_results
+        else:
+            raise ValueError
+
+    mock_perfect_calc_results = mocker.Mock(spec=CalcResults)
+    mock_loadfn = mocker.patch(
+        "pydefect.cli.main_functions.loadfn", side_effect=side_effect)
+    mock_unitcell = mocker.Mock(spec=Unitcell)
+    mock_make_efnv = mocker.patch(
+        "pydefect.cli.main_functions.make_efnv_correction")
+    mock_efnv = mocker.Mock(spec=ExtendedFnvCorrection, autospec=True)
+    mock_make_efnv.return_value = mock_efnv
+
+    mock_site_pot_plotter = mocker.patch(
+        "pydefect.cli.main_functions.SitePotentialMplPlotter")
+    plotter = mock_site_pot_plotter.from_efnv_corr.return_value
+    args = Namespace(dirs=[Path("Va_O1_2")],
+                     perfect_calc_results=mock_perfect_calc_results,
+                     unitcell=mock_unitcell)
+
+    make_efnv_correction_main_func(args)
+    mock_loadfn.assert_any_call(Path("Va_O1_2") / "defect_entry.json")
+    mock_loadfn.assert_any_call(Path("Va_O1_2") / "calc_results.json")
+
+    mock_make_efnv.assert_called_with(
+        mock_defect_entry.charge, mock_calc_results, mock_perfect_calc_results,
+        mock_unitcell.dielectric_constant)
+    mock_efnv.to_json_file.assert_called_with(
+        Path("Va_O1_2") / "correction.json")
+
+    mock_site_pot_plotter.from_efnv_corr.assert_called_with(
+        title="Va_O1_2", efnv_correction=mock_efnv)
+    plotter.construct_plot.assert_called_once_with()
+    plotter.return_value.plt.savefig.assert_called_once_with(
+        fname=Path("Va_O1_2") / "correction.pdf")
+
+
+def test_make_band_edge_states(mocker):
+    mock_perf_bes = mocker.Mock(spec=PerfectBandEdgeState, autospec=True)
+    args = Namespace(dirs=[Path("Va_O1_2")], p_state=mock_perf_bes)
+    mocker_beoi = mocker.Mock(spec=BandEdgeOrbitalInfos, autospec=True)
+
+    def side_effect(key):
+        if str(key) == "Va_O1_2/band_edge_orbital_infos.json":
+            return mocker_beoi
+        else:
+            raise ValueError
+
+    mocker_loadfn = mocker.patch(
+        "pydefect.cli.main_functions.loadfn", side_effect=side_effect)
+    mocker_mbes = mocker.patch(
+        "pydefect.cli.main_functions.make_band_edge_states")
+    make_band_edge_states_main_func(args)
+    mocker_mbes.assert_called_with(mocker_beoi, mock_perf_bes)
+
+
+
