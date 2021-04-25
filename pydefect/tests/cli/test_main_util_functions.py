2bb2b82e21feb741e31e2db7390e5eef603a2746
yuuukuma <yuuukuma@gmail.com>
2021-04-25 14:06:57 +0900

    Add test_main_util_functions.py

---
+++ pydefect/tests/cli/test_main_util_functions.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+from argparse import Namespace
+from pathlib import Path
+
+from pydefect.analyzer.calc_results import CalcResults
+from pydefect.chem_pot_diag.chem_pot_diag import CompositionEnergies, \
+    CompositionEnergy
+from pydefect.cli.main_util_functions import composition_energies_from_mp, \
+    make_gkfo_correction_from_vasp
+from pydefect.corrections.efnv_correction import ExtendedFnvCorrection
+from pymatgen.core import Composition
+
+
+def test_composition_energies_from_mp(mocker, tmpdir):
+    tmpdir.chdir()
+    args = Namespace(elements=["H"], atom_energy_yaml="a.yaml")
+    mock = mocker.patch(
+        "pydefect.cli.main_util_functions.make_composition_energies_from_mp")
+    mock.return_value = CompositionEnergies(
+        {Composition("H"): CompositionEnergy(1.0, "mp-1")})
+    composition_energies_from_mp(args)
+    mock.assert_called_once_with(elements=["H"], atom_energy_yaml="a.yaml")
+    actual = Path("composition_energies.yaml").read_text()
+    expected = """H1:
+  energy: 1.0
+  source: mp-1
+"""
+    assert actual == expected
+
+
+def test_make_gkfo_correction_from_vasp(tmpdir, mocker):
+    mock_i_correction = mocker.Mock(spec=ExtendedFnvCorrection, autospec=True)
+    mock_i_calc_results = mocker.Mock(spec=CalcResults, autospec=True)
+    mock_f_calc_results = mocker.Mock(spec=CalcResults, autospec=True)
+    mock_unitcell = mocker.Mock()
+
+    mock_pot_plotter = mocker.patch(
+        "pydefect.cli.main_util_functions.SitePotentialMplPlotter")
+    mock_make_gkfo = mocker.patch(
+        "pydefect.cli.main_util_functions.make_gkfo_correction")
+
+    args = Namespace(
+        initial_efnv_correction=mock_i_correction,
+        initial_calc_results=mock_i_calc_results,
+        final_calc_results=mock_f_calc_results,
+        charge_diff=1,
+        unitcell=mock_unitcell)
+
+    make_gkfo_correction_from_vasp(args)
+    mock_make_gkfo.assert_called_with(
+        efnv_correction=mock_i_correction,
+        additional_charge=1,
+        final_calc_results=mock_f_calc_results,
+        initial_calc_results=mock_i_calc_results,
+        diele_tensor=mock_unitcell.dielectric_constant,
+        ion_clamped_diele_tensor=mock_unitcell.ele_dielectric_const)
+
+
