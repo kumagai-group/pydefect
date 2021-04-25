2c6557b2a1dc476bc9734e89568f7852d69a4068
yuuukuma <yuuukuma@gmail.com>
2021-04-25 12:32:00 +0900

    Refactor main_vasp_util.py

--- pydefect/cli/vasp/main_vasp_util.py
+++ pydefect/cli/vasp/main_vasp_util.py
@@ -7,10 +7,8 @@ import sys
 import warnings
 from pathlib import Path
 
-from monty.serialization import loadfn
-from pydefect import __version__
+from pydefect.cli.main import epilog, description
 from pydefect.cli.vasp.main_vasp_util_functions import \
-    make_gkfo_correction_from_vasp, \
     calc_defect_charge_info
 from pymatgen.io.vasp.inputs import UnknownPotcarWarning
 
@@ -18,43 +16,11 @@ warnings.simplefilter('ignore', UnknownPotcarWarning)
 
 
 def parse_args_main_vasp_util(args):
-    parser = argparse.ArgumentParser(
-        description="""                            
-    pydefect is a package that helps researchers to do first-principles point 
-    defect calculations with the VASP code.
-    This command provide some utilities related to the VASP calculations""",
-        epilog=f"Author: Yu Kumagai Version: {__version__}")
+    parser = argparse.ArgumentParser(epilog=epilog,
+                                     description=description + """      
+    This command provides some utilities related to the VASP calculations""")
 
     subparsers = parser.add_subparsers()
-
-    # ++ parent parser: dirs
-    dirs_parser = argparse.ArgumentParser(
-        description="", add_help=False)
-    dirs_parser.add_argument(
-        "-d", "--dirs", nargs="+", type=Path,
-        help="Directory paths to be parsed.")
-
-    # ++ parent parser: supercell_info
-    si_parser = argparse.ArgumentParser(
-        description="", add_help=False)
-    si_parser.add_argument(
-        "-s", "--supercell_info", required=True, type=loadfn,
-        default="supercell_info.json",
-        help="Path to the supercell_info.json file.")
-
-    # ++ parent parser: pcr
-    pcr_parser = argparse.ArgumentParser(
-        description="", add_help=False)
-    pcr_parser.add_argument(
-        "-pcr", "--perfect_calc_results", required=True, type=loadfn,
-        help="Path to the calc_results.json for the perfect supercell.")
-
-    # ++ parent parser: unitcell
-    unitcell_parser = argparse.ArgumentParser(
-        description="", add_help=False)
-    unitcell_parser.add_argument(
-        "-u", "--unitcell", required=True, type=loadfn,
-        help="Path to the unitcell.json file.")
 
     # -- calc defect charge info -----------------------------------------------
     parser_calc_def_charge_info = subparsers.add_parser(
@@ -75,29 +41,6 @@ def parse_args_main_vasp_util(args):
 
     parser_calc_def_charge_info.set_defaults(func=calc_defect_charge_info)
 
-    # -- gkfo correction ------------------------------------------------
-    parser_gkfo = subparsers.add_parser(
-        name="gkfo",
-        description="Generate GKFO correction files.",
-        parents=[unitcell_parser],
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        aliases=['gkfo'])
-
-    parser_gkfo.add_argument(
-        "-iefnv", "--initial_efnv_correction", required=True, type=loadfn,
-        help="Path to the initial efnv correction.json file.")
-    parser_gkfo.add_argument(
-        "-icr", "--initial_calc_results", required=True, type=loadfn,
-        help="Path to the initial calc_results.json file.")
-    parser_gkfo.add_argument(
-        "-fcr", "--final_calc_results", required=True, type=loadfn,
-        help="Path to the final calc_results.json file.")
-    parser_gkfo.add_argument(
-        "-cd", "--charge_diff", required=True, type=int,
-        help="Charge difference of final state from initial state.")
-
-    parser_gkfo.set_defaults(func=make_gkfo_correction_from_vasp)
-    # ------------------------------------------------------------------------
     return parser.parse_args(args)
 
 
--- pydefect/tests/cli/vasp/test_main_vasp_util.py
+++ pydefect/tests/cli/vasp/test_main_vasp_util.py
@@ -3,11 +3,7 @@
 from argparse import Namespace
 from pathlib import Path
 
-from pydefect.analyzer.calc_results import CalcResults
-from pydefect.analyzer.unitcell import Unitcell
 from pydefect.cli.vasp.main_vasp_util import parse_args_main_vasp_util
-from pydefect.corrections.efnv_correction import \
-    ExtendedFnvCorrection
 
 
 def test_calc_defect_charge_info():
@@ -21,37 +17,4 @@ def test_calc_defect_charge_info():
         func=parsed_args.func)
     assert parsed_args == expected
 
-
-def test_gkfo_correction(mocker):
-    mock_i_correction = mocker.Mock(spec=ExtendedFnvCorrection, autospec=True)
-    mock_i_calc_results = mocker.Mock(spec=CalcResults, autospec=True)
-    mock_f_calc_results = mocker.Mock(spec=CalcResults, autospec=True)
-    mock_unitcell = mocker.Mock(spec=Unitcell, autospec=True)
-
-    def side_effect(filename):
-        if filename == "a/correction.json":
-            return mock_i_correction
-        elif filename == "a/calc_results.json":
-            return mock_i_calc_results
-        elif filename == "a/absorption/calc_results.json":
-            return mock_f_calc_results
-        elif filename == "unitcell.json":
-            return mock_unitcell
-        else:
-            raise ValueError
-
-    mocker.patch("pydefect.cli.vasp.main_vasp_util.loadfn",
-                 side_effect=side_effect)
-    parsed_args = parse_args_main_vasp_util([
-        "gkfo", "-iefnv", "a/correction.json", "-cd", "1",
-        "-icr", "a/calc_results.json", "-fcr", "a/absorption/calc_results.json",
-        "-u", "unitcell.json"])
-    expected = Namespace(
-        initial_efnv_correction=mock_i_correction,
-        initial_calc_results=mock_i_calc_results,
-        final_calc_results=mock_f_calc_results,
-        charge_diff=1,
-        unitcell=mock_unitcell,
-        func=parsed_args.func)
-    assert parsed_args == expected
 
