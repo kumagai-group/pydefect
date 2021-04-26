3f85610c246075fab0d2fec24788309431d8880e
yuuukuma <yuuukuma@gmail.com>
2021-04-26 10:00:38 +0900

    Refactor

--- pydefect/tests/analyzer/test_make_defect_energy_summary.py
+++ pydefect/tests/analyzer/test_make_defect_energy_summary.py
@@ -9,12 +9,12 @@ from pydefect.chem_pot_diag.chem_pot_diag import TargetVertices, TargetVertex
 
 
 def test_make_defect_energy_summary(mocker):
-    de1 = DefectEnergy(0.0, {"PC correction": 2.0}, False)
-    de2 = DefectEnergy(1.0, {"PC correction": 3.0}, True)
-    defect_infos = [DefectEnergyInfo("Va_Mg1", 0, {"Mg": -1}, de1),
-                    DefectEnergyInfo("Va_Mg1", 1, {"Mg": -1}, de2)]
-    target_vertices = TargetVertices(target="MgO",
-                                     vertices={"A": TargetVertex({"Mg": 5.0})})
+    energy1 = DefectEnergy(0.0, {"PC correction": 2.0}, False)
+    energy2 = DefectEnergy(1.0, {"PC correction": 3.0}, True)
+    defect_infos = [DefectEnergyInfo("Va_Mg1", 0, {"Mg": -1}, energy1),
+                    DefectEnergyInfo("Va_Mg1", 1, {"Mg": -1}, energy2)]
+    target_vertices = TargetVertices(
+        target="MgO", vertices={"A": TargetVertex({"Mg": 5.0})})
 
     unitcell = mocker.Mock()
     perf_be_state = mocker.Mock()
@@ -22,9 +22,9 @@ def test_make_defect_energy_summary(mocker):
     actual = make_defect_energy_summary(defect_infos, target_vertices, unitcell,
                                         perf_be_state)
 
-    defect_energies = {"Va_Mg1": DefectEnergies(atom_io={"Mg": -1},
-                                                charges=[0, 1],
-                                                defect_energies=[de1, de2])}
+    defect_energies = {"Va_Mg1": DefectEnergies(
+        atom_io={"Mg": -1}, charges=[0, 1], defect_energies=[energy1, energy2])}
+
     expected = DefectEnergySummary(title=unitcell.system,
                                    defect_energies=defect_energies,
                                    rel_chem_pots={"A": {"Mg": 5.0}},
