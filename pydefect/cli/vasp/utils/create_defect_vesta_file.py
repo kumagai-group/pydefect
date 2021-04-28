78ac953049752975a1eb09f455d08e0c8fc34577
yuuukuma <yuuukuma@gmail.com>
2021-04-28 22:49:02 +0900

    Update defect_vesta_file

--- pydefect/cli/vasp/utils/create_defect_vesta_file.py
+++ pydefect/cli/vasp/utils/create_defect_vesta_file.py
@@ -1,99 +1,99 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020 Kumagai group.
-import fire
-from monty.serialization import loadfn
-from pydefect.analyzer.calc_results import CalcResults
 from pydefect.analyzer.defect_structure_info import DefectStructureInfo, \
     fold_coords_in_structure
 from pydefect.analyzer.vesta.create_vesta_file import VestaFile
 from pydefect.defaults import defaults
-from pydefect.input_maker.defect_entry import DefectEntry
-from pydefect.input_maker.supercell_info import SupercellInfo
 from pymatgen.core import Structure, DummySpecies, Element
 
 
-def make_defect_vesta_file(defect_structure: Structure,
-                           def_str_info: DefectStructureInfo,
-                           title: str = None,
-                           cutoff: float = defaults.show_structure_cutoff,
-                           show_disp_min: float = 0.1,
-                           disp_enhance_factor: float = 3.0,
-                           initial_filename: str = "defect_initial.vesta",
-                           final_filename: str = "defect.vesta"):
-    species, initial_coords, final_coords, site_info = [], [], [], []
-    vectors, vector_colors = {}, []
-    i = 1
-    for f_idx, (s, disp) in enumerate(zip(defect_structure, def_str_info.displacements)):
-        if disp and disp.distance_from_defect < cutoff:
-            species.append(disp.specie)
-            initial_coords.append(disp.original_pos)
-            final_coords.append(s.frac_coords)
-            info = f"{round(disp.distance_from_defect, 1)}"
+class MakeDefectVestaFile:
+    def __init__(self,
+                 defect_structure: Structure,
+                 defect_structure_info: DefectStructureInfo,
+                 cutoff: float = defaults.show_structure_cutoff,
+                 min_displace_w_arrows: float = 0.1,
+                 arrow_factor: float = 3.0,
+                 title: str = None):
+        self.defect_structure = defect_structure
+        self.lattice = defect_structure.lattice
 
-            if disp and disp.displace_distance > show_disp_min:
-                vectors[i] = (v * disp_enhance_factor for v in disp.disp_vector)
-                # use 200 as max instead of 255 since (255, 255, 255) is white.
-                rgb = int(200 * disp.angle / 180)
-                vector_colors.append((rgb, rgb, 100))
-                info += f"_{round(disp.displace_distance, 2)}"
+        self.removed_by_sub = defect_structure_info.site_diff.removed_by_sub
+        self.inserted_by_sub = defect_structure_info.site_diff.inserted_by_sub
+        self.removed = defect_structure_info.site_diff.removed
+        self.inserted = defect_structure_info.site_diff.inserted
+        self.is_complex = defect_structure_info.site_diff.is_complex_defect
 
-            site_info.append(info)
-            i += 1
+        self.center = defect_structure_info.center
+        self.displacements = defect_structure_info.displacements
 
-    initial_local = Structure(defect_structure.lattice, species, initial_coords)
-    final_local = Structure(defect_structure.lattice, species, final_coords)
-    for s, i in zip(final_local, site_info):
-        s.properties["name"] = i
-    for _, specie, fcoord in def_str_info.site_diff.inserted:
-        final_local.append(Element(specie), coords=fcoord)
-        final_local[-1].properties["name"] = f"{specie}_i"
+        self.cutoff = cutoff
+        self.min_displace_w_arrows = min_displace_w_arrows
+        self.arrow_factor = arrow_factor
 
-    for (_, i_specie, i_fcoord), (_, r_specie, r_fcoord) in zip(def_str_info.site_diff.inserted_by_sub, def_str_info.site_diff.removed_by_sub):
-        initial_local.append(Element(r_specie), coords=r_fcoord)
-        final_local.append(Element(i_specie), coords=i_fcoord)
-        final_local[-1].properties["name"] = f"{i_specie}_{r_specie}"
+        self._make_local_species_info()
+        self._initial_local = self.make_initial_structure()
+        self._final_local = self.make_final_structure()
 
-    for _, specie, fcoord in def_str_info.site_diff.removed:
-        initial_local.append(Element(specie), coords=fcoord)
-        final_local.append(DummySpecies(), coords=fcoord)
-        final_local[-1].properties["name"] = f"Va_{specie}"
+        boundary = sum([[x - 0.5, x + 0.5]
+                        for x in defect_structure_info.center], [])
 
-    if def_str_info.site_diff.is_complex_defect:
-        final_local.append(DummySpecies(), coords=def_str_info.center)
-        final_local[-1].properties["name"] = f"center"
+        self.initial_vesta = \
+            VestaFile(self._initial_local, title, boundary=boundary)
+        self.final_vesta = \
+            VestaFile(self._final_local, title, vectors=self._vectors,
+                      vector_colors=self._vector_colors, boundary=boundary)
 
-    fold_coords_in_structure(initial_local, def_str_info.center)
-    fold_coords_in_structure(final_local, def_str_info.center)
+    def make_initial_structure(self):
+        result = Structure(self.lattice, self._species, self._initial_coords)
+        for _, specie, coord in self.removed_by_sub + self.removed:
+            result.append(specie, coords=coord)
+        fold_coords_in_structure(result, self.center)
+        return result
 
-    boundary = sum([[x - 0.5, x + 0.5] for x in def_str_info.center], [])
+    def make_final_structure(self):
+        result = Structure(self.lattice, self._species, self._final_coords,
+                           site_properties={"name": self._site_info})
+        # inserted
+        for _, specie, coords in self.inserted:
+            result.append(specie, coords, properties={"name": f"{specie}_i"})
+        # substituted
+        for (_, i_specie, i_fcoord), (_, r_specie, r_fcoord) \
+                in zip(self.inserted_by_sub, self.removed_by_sub):
+            result.append(i_specie, i_fcoord,
+                          properties={"name": f"{i_specie}_{r_specie}"})
+        # removed. DummySpecies come later.
+        for _, specie, coords in self.removed:
+            result.append(DummySpecies(), coords,
+                          properties={"name": f"Va_{specie}"})
 
-    VestaFile(initial_local, title, boundary=boundary).write_file(initial_filename)
-    VestaFile(final_local, title, vectors=vectors, vector_colors=vector_colors, boundary=boundary).write_file(final_filename)
+        if self.is_complex:
+            result.append(DummySpecies(), self.center,
+                          properties={"name": "center"})
 
+        fold_coords_in_structure(result, self.center)
+        return result
 
-def make_defect_vesta_file_via_fire(supercell_info_json,
-                                    defect_entry_json,
-                                    calc_results_json,
-                                    defect_str_info_json,
-                                    cutoff: float = 5.0,
-                                    show_disp_min: float = 0.1,
-                                    disp_enhance_factor: float = 3.0,
-                                    initial_filename: str = "defect_initial.vesta",
-                                    final_filename: str = "defect.vesta"):
-    supercell_info: SupercellInfo = loadfn(supercell_info_json)
-    defect_entry: DefectEntry = loadfn(defect_entry_json)
-    calc_results: CalcResults = loadfn(calc_results_json)
-    defect_str_info = loadfn(defect_str_info_json)
-    title = f"{defect_entry.full_name} in {supercell_info.structure.formula}"
-    make_defect_vesta_file(calc_results.structure,
-                           defect_str_info,
-                           title,
-                           cutoff,
-                           show_disp_min,
-                           disp_enhance_factor,
-                           initial_filename,
-                           final_filename)
+    def _make_local_species_info(self):
+        self._species, self._initial_coords, self._final_coords = [], [], []
+        self._site_info, self._vectors, self._vector_colors = [], {}, []
 
+        specie_idx = 1
+        for specie, disp in zip(self.defect_structure, self.displacements):
+            if disp and disp.distance_from_defect < self.cutoff:
+                self._species.append(Element(disp.specie))
+                self._initial_coords.append(disp.original_pos)
+                self._final_coords.append(specie.frac_coords)
+                info = f"{round(disp.distance_from_defect, 1)}"
 
-if __name__ == '__main__':
-    fire.Fire(make_defect_vesta_file_via_fire)
+                if disp and disp.displace_distance > self.min_displace_w_arrows:
+                    self._vectors[specie_idx] = \
+                        (v * self.arrow_factor for v in disp.disp_vector)
+                    # max=200 instead of 255 since (255, 255, 255) is white.
+                    rgb = int(200 * disp.angle / 180)
+                    self._vector_colors.append((rgb, rgb, 100))
+                    info += f"_{round(disp.displace_distance, 2)}"
+
+                self._site_info.append(info)
+                specie_idx += 1
+
--- pydefect/tests/cli/vasp/utils/test_create_defect_vesta_file.py
+++ pydefect/tests/cli/vasp/utils/test_create_defect_vesta_file.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020 Kumagai group.
+from pathlib import Path
 
 from pydefect.analyzer.defect_structure_comparator import SiteDiff
 from pydefect.analyzer.defect_structure_info import DefectStructureInfo, \
     Displacement
 from pydefect.cli.vasp.utils.create_defect_vesta_file import \
-    make_defect_vesta_file
+    MakeDefectVestaFile
 from pymatgen.core import Structure, Lattice
 
 
@@ -64,4 +65,107 @@ def test_defect_vesta_file(tmpdir):
                 [0.5, 0.5, 0.501],
                 [0.0, 0.0, 0.0001]])
     tmpdir.chdir()
-    make_defect_vesta_file(defect_structure=structure, def_str_info=def_str_info, disp_enhance_factor=100)
+    x = MakeDefectVestaFile(defect_structure=structure, defect_structure_info=def_str_info, arrow_factor=100)
+    x.initial_vesta.write_file("defect_initial.vesta")
+    x.final_vesta.write_file("defect.vesta")
+
+    expected_initial = """#VESTA_FORMAT_VERSION 3.5.0
+TITLE
+Li1 Be1 H1
+
+CELLP
+10.000000 10.000000 10.000000 90.000000 90.000000 90.000000
+
+STRUC
+1 Be Be1 1.0 0.500000 0.500000 0.500000
+ 0.0 0.0 0.0 
+2 Li Li2 1.0 0.500000 0.500000 0.500000
+ 0.0 0.0 0.0 
+3 H H3 1.0 0.250000 0.250000 0.250000
+ 0.0 0.0 0.0 
+ 0 0 0 0 0 
+
+BOUND
+-0.120000 0.880000 -0.125000 0.875000 -0.124825 0.875175
+ 0 0 0 0 0 
+
+SBOND
+1 Li Be 0.0 1.788  0  0  1  0  1 0.12   2.000 161  33 246
+2 Li H 0.0  1.08  0  0  1  0  1 0.12   2.000 161  33 246
+3 Be Li 0.0 1.788  0  0  1  0  1 0.12   2.000 161  33 246
+4 Be H 0.0 0.708  0  0  1  0  1 0.12   2.000 161  33 246
+5 H Li 0.0  1.08  0  0  1  0  1 0.12   2.000 161  33 246
+6 H Be 0.0 0.708  0  0  1  0  1 0.12   2.000 161  33 246
+ 0 0 0 0 
+
+SITET
+ 1  Be1  0.5 94 215 123 94 215 123 204 1
+ 2  Li2  0.5 134 224 116 134 224 116 204 1
+ 3  H3  0.5 255 204 204 255 204 204 204 1
+ 0 0 0 0 
+
+
+
+STYLE
+VECTS  1.0
+SECTS  64  1
+SECTP 
+   1 0 0  0.00000E+00  0.00000E+00  -1.00000E+01  1.00000E+01
+UCOLP 
+ 0  2  1.000   0   0   0
+ATOMS  1  0  1
+BONDP 
+   1  16  0.12  1.000 127 127 127"""
+    assert Path("defect_initial.vesta").read_text() == expected_initial
+
+    expected_final = """#VESTA_FORMAT_VERSION 3.5.0
+TITLE
+X2 Be2 H1
+
+CELLP
+10.000000 10.000000 10.000000 90.000000 90.000000 90.000000
+
+STRUC
+1 Be 2.1 1.0 0.500000 0.500000 0.501000
+ 0.0 0.0 0.0 
+2 H H_i 1.0 0.270000 0.250000 0.250000
+ 0.0 0.0 0.0 
+3 Be Be_Li 1.0 0.500000 0.500000 0.500900
+ 0.0 0.0 0.0 
+4 XX Va_H 1.0 0.250000 0.250000 0.250000
+ 0.0 0.0 0.0 
+5 XX center 1.0 0.380000 0.375000 0.375175
+ 0.0 0.0 0.0 
+ 0 0 0 0 0 
+
+BOUND
+-0.120000 0.880000 -0.125000 0.875000 -0.124825 0.875175
+ 0 0 0 0 0 
+
+SBOND
+4 Be H 0.0 0.708  0  0  1  0  1 0.12   2.000 161  33 246
+6 H Be 0.0 0.708  0  0  1  0  1 0.12   2.000 161  33 246
+ 0 0 0 0 
+
+SITET
+ 1  2.1  0.5 94 215 123 94 215 123 204 1
+ 2  H_i  0.5 255 204 204 255 204 204 204 1
+ 3  Be_Li  0.5 94 215 123 94 215 123 204 1
+ 4  Va_H  0.5 30 30 30 30 30 30 204 1
+ 5  center  0.3 30 30 30 30 30 30 204 0
+ 0 0 0 0 
+
+
+
+STYLE
+VECTS  1.0
+SECTS  64  1
+SECTP 
+   1 0 0  0.00000E+00  0.00000E+00  -1.00000E+01  1.00000E+01
+UCOLP 
+ 0  2  1.000   0   0   0
+ATOMS  1  0  1
+BONDP 
+   1  16  0.12  1.000 127 127 127"""
+
+    assert Path("defect.vesta").read_text() == expected_final
