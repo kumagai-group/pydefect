b3c3096c47060c9845259d400f7c1a96eb2c939f
yuuukuma <yuuukuma@gmail.com>
2021-04-28 13:03:14 +0900

    Modify create_local_extrema.py

--- pydefect/cli/vasp/create_local_extrema.py
+++ pydefect/cli/vasp/create_local_extrema.py
@@ -1,135 +1,74 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020 Kumagai group.
-from dataclasses import dataclass, asdict
-from typing import List, Tuple, Union
+from itertools import groupby
+from typing import List
 
-import fire
-from pydefect.input_maker.local_extrema import LocalExtrema, CoordInfo, \
-    VolumetricDataAnalyzeParams
-from pydefect.util.structure_tools import Distances, Coordination
-from pymatgen import DummySpecies
+from pandas import DataFrame
+from pydefect.input_maker.local_extrema import VolumetricDataLocalExtrema, \
+    CoordInfo, VolumetricDataAnalyzeParams
+from pydefect.util.structure_tools import Distances
 from pymatgen.analysis.defects.utils import ChargeDensityAnalyzer
 from pymatgen.core import Element, Structure
-from pymatgen.io.vasp import Chgcar, VolumetricData
+from pymatgen.io.vasp import VolumetricData
 from vise.util.structure_symmetrizer import StructureSymmetrizer
-import numpy as np
-from vise.util.typing import Coords
-
-
-def interstitials_from_volumetric_data(
-        volumetric_data: VolumetricData,
-        find_min: bool = True,
-        threshold_frac: float = None,
-        threshold_abs: float = None,
-        min_dist: float = 0.5,
-        tol: float = 0.5,
-        radius: float = 0.4) -> None:
-
-    cda = ChargeDensityAnalyzer(chgcar=volumetric_data)
-    cda.get_local_extrema(threshold_frac=threshold_frac,
-                          threshold_abs=threshold_abs,
-                          find_min=find_min)
-    cda.sort_sites_by_integrated_chg(r=radius)
-    # Remove sites near host atoms.
-    cda.remove_collisions(min_dist)
-    # Cluster interstitials that are too close together using a tol.
-    try:
-        cda.cluster_nodes(tol=tol)
-    except ValueError:
-        # error raised when only a single local extreme point.
-        pass
-    print(cda.extrema_df)
-
-    structure: Structure = volumetric_data.structure.copy()
-    start_index = len(structure)
-    end_index = len(structure) + len(cda.extrema_coords)
-    interstitial_indices = [i for i in range(start_index, end_index)]
-    for coords in cda.extrema_coords:
-        structure.append(Element.H, coords)
-    symmetrizer = StructureSymmetrizer(structure)
-    equiv_atoms = symmetrizer.spglib_sym_data["equivalent_atoms"]
-
-    print(f"Host symmetry {symmetrizer.spglib_sym_data['international']}")
-    print("++ Inequivalent indices and site symmetries ++")
-    orig_num_atoms = len(volumetric_data.structure)
-
-    structure_for_visualize: Structure = volumetric_data.structure.copy()
-
-    for i, ii in enumerate(interstitial_indices):
-        if ii == equiv_atoms[ii]:
-            idx = orig_num_atoms + i
-            coords = structure[idx].frac_coords
-
-            def element():
-                for z in range(1, 100):
-                    e = Element.from_Z(z)
-                    if e not in structure_for_visualize.composition:
-                        yield e
-
-            structure_for_visualize.append(next(element()), coords)
-
-            idx_coords = \
-                f"{i:>3} {coords[0]:8.4f} {coords[1]:8.4f} {coords[2]:8.4f}"
-            print(idx_coords,
-                  symmetrizer.spglib_sym_data["site_symmetry_symbols"][ii])
-
-        structure_for_visualize.to(filename="POSCAR-w-interstitials")
 
 
 def make_local_extrema_from_volumetric_data(
         volumetric_data: VolumetricData,
         params: VolumetricDataAnalyzeParams = VolumetricDataAnalyzeParams(),
         info: str = None,
-        find_min: bool = True) -> LocalExtrema:
-    extrema = extrema_coords(volumetric_data, find_min, **asdict(params))
-
-    structure: Structure = volumetric_data.structure.copy()
-    for coords in extrema:
-        structure.append(Element.H, coords)
-    symmetrizer = StructureSymmetrizer(structure)
-    equiv_atoms = symmetrizer.spglib_sym_data["equivalent_atoms"]
-
-    extrema_points = []
-    start_index, end_index = len(volumetric_data.structure), len(structure)
-    interstitial_indices = [i for i in range(start_index, end_index)]
-    for i, ii in enumerate(interstitial_indices):
-        if ii == equiv_atoms[ii]:
-            idx = start_index + i
-            fcoords = structure[idx].frac_coords
-            site_sym = symmetrizer.spglib_sym_data["site_symmetry_symbols"][ii]
-            coordination = Distances(structure, fcoords).coordination(include_on_site=False)
-            extrema_points.append(CoordInfo(fcoords, site_sym, coordination, 1.0))
-    return LocalExtrema(volumetric_data.structure, find_min, extrema_points, info)
+        find_min: bool = True) -> VolumetricDataLocalExtrema:
+    extrema = extrema_coords(volumetric_data, find_min, params)
+    coord_infos = find_inequivalent_coords(volumetric_data.structure,
+                                           extrema)
+    return VolumetricDataLocalExtrema(volumetric_data.structure,
+                                      is_min=find_min,
+                                      extrema_points=coord_infos,
+                                      info=info,
+                                      params=params)
 
 
 def find_inequivalent_coords(structure: Structure,
-                             frac_coords: List[np.array],
-                             quantities: List[float]) -> List[CoordInfo]:
+                             df: DataFrame) -> List[CoordInfo]:
     result = []
-    structure = Structure.from_dict(structure.as_dict())
+    initial_sg = StructureSymmetrizer(structure).sg_number
+    added_structure = Structure.from_dict(structure.as_dict())
     start_index = len(structure)
-    for coords in frac_coords:
+    for _, column in df.iterrows():
+        coords = [column.a, column.b, column.c]
         assert Element.Og not in structure.composition.elements
-        structure.append(Element.Og, coords)
-    end_index = len(structure)
+        added_structure.append(Element.Og, coords)
+    end_index = len(added_structure)
 
-    symmetrizer = StructureSymmetrizer(structure)
-    equiv_atoms = symmetrizer.spglib_sym_data["equivalent_atoms"]
-    interstitial_indices = [i for i in range(start_index, end_index)]
-    for i, ii in enumerate(interstitial_indices):
-        if ii == equiv_atoms[ii]:
-            idx = start_index + i
-            fcoords = structure[idx].frac_coords
-            site_sym = symmetrizer.spglib_sym_data["site_symmetry_symbols"][ii]
-            coordination = Distances(structure, fcoords).coordination(include_on_site=False)
-            result.append(CoordInfo(tuple(fcoords), site_sym, coordination, 1.0))
+    symmetrizer = StructureSymmetrizer(added_structure)
+    assert initial_sg == symmetrizer.sg_number
 
+    sym_data = symmetrizer.spglib_sym_data
+    _indices = [i for i in range(start_index, end_index)]
+    repr_atom_pairs = zip(sym_data["equivalent_atoms"][_indices], _indices)
+
+    for _, equiv_sites in groupby(repr_atom_pairs, key=lambda x: x[0]):
+        coords, quantity = [], []
+        for repr_idx, atom_idx in equiv_sites:
+            fcoord = added_structure[atom_idx].frac_coords
+            coords.append(tuple(fcoord))
+            quantity.append(df["Charge Density"][atom_idx - start_index])
+            if repr_idx == atom_idx:
+                site_sym = sym_data["site_symmetry_symbols"][atom_idx]
+                distances = Distances(added_structure, fcoord)
+                coordination = distances.coordination()
+
+        coord_info = CoordInfo(site_symmetry=site_sym,
+                               coordination=coordination,
+                               frac_coords=coords,
+                               quantities=quantity)
+        result.append(coord_info)
     return result
 
 
 def extrema_coords(volumetric_data: VolumetricData,
                    find_min: bool,
-                   params: VolumetricDataAnalyzeParams) -> List[np.array]:
+                   params: VolumetricDataAnalyzeParams) -> DataFrame:
     result = ChargeDensityAnalyzer(chgcar=volumetric_data)
     result.get_local_extrema(threshold_frac=params.threshold_frac,
                              threshold_abs=params.threshold_abs,
@@ -143,13 +82,5 @@ def extrema_coords(volumetric_data: VolumetricData,
     except ValueError:
         # error raised when only a single local extreme point.
         pass
-    return result.extrema_coords
+    return result.extrema_df
 
-
-def interstitials_from_charge_density(aeccar0, aeccar2, **kwargs):
-    aeccar = Chgcar.from_file(aeccar0) + Chgcar.from_file(aeccar2)
-    interstitials_from_volumetric_data(aeccar, **kwargs)
-
-
-if __name__ == '__main__':
-    fire.Fire(interstitials_from_charge_density)
--- pydefect/tests/cli/vasp/test_create_local_extrema.py
+++ pydefect/tests/cli/vasp/test_create_local_extrema.py
@@ -1,31 +1,38 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020 Kumagai group.
-
-from pydefect.cli.vasp.create_local_extrema import \
-    interstitials_from_volumetric_data, extrema_coords, \
-    VolumetricDataAnalyzeParams, find_inequivalent_coords
+from pandas import DataFrame
+from pydefect.cli.vasp.create_local_extrema import extrema_coords, \
+    VolumetricDataAnalyzeParams, find_inequivalent_coords, \
+    make_local_extrema_from_volumetric_data
 from pydefect.input_maker.local_extrema import CoordInfo
 from pydefect.util.structure_tools import Coordination
 from pymatgen.io.vasp import Chgcar
-import numpy as np
 
 
-def test_interstitials_from_charge_density(vasp_files):
+def test_make_local_extrema_from_volumetric_data(vasp_files):
     aeccar0 = Chgcar.from_file(str(vasp_files / "NaMgF3_AECCAR0"))
     aeccar2 = Chgcar.from_file(str(vasp_files / "NaMgF3_AECCAR2"))
     aeccar = aeccar0 + aeccar2
     aeccar.write_file("CHGCAR")
-    interstitials_from_volumetric_data(aeccar)
+    make_local_extrema_from_volumetric_data(aeccar)
 
 
 def test_find_inequivalent_coords(simple_cubic):
-    actual = find_inequivalent_coords(simple_cubic,
-                                      [np.array([0.1, 0.0, 0.0])],
-                                      [1.0])
-    expected = [CoordInfo((0.1, 0.0, 0.0), "4mm",
-                          coordination=Coordination({"H": [0.1]}, 0.13, [0]),
-                          quantity=1.0)]
-    assert actual[0] == expected[0]
+    df = DataFrame([[0.1, 0.0, 0.0, 1.0],
+                    [0.0, 0.1, 0.0, 1.0],
+                    [0.0, 0.0, 0.1, 1.0],
+                    [0.9, 0.0, 0.0, 1.0],
+                    [0.0, 0.9, 0.0, 1.0],
+                    [0.0, 0.0, 0.9, 1.0]],
+                   columns=["a", "b", "c", "Charge Density"])
+    actual = find_inequivalent_coords(simple_cubic, df)
+    expected = CoordInfo(site_symmetry="4m.m",
+                         coordination=Coordination({"H": [0.1]}, 0.13, [0]),
+                         frac_coords=[(0.1, 0.0, 0.0), (0.0, 0.1, 0.0),
+                                      (0.0, 0.0, 0.1), (0.9, 0.0, 0.0),
+                                      (0.0, 0.9, 0.0), (0.0, 0.0, 0.9)],
+                         quantities=[1.0]*6)
+    assert actual[0] == expected
 
 
 def test_extrema_coords(vasp_files):
@@ -34,11 +41,6 @@ def test_extrema_coords(vasp_files):
     aeccar = aeccar0 + aeccar2
     actual = extrema_coords(volumetric_data=aeccar, find_min=True,
                             params=VolumetricDataAnalyzeParams())
+    print(actual.index)
     assert len(actual) == 36
-    np.testing.assert_array_almost_equal(actual[0],
-                                         np.array([0.2, 0.75, 0.908333]))
-
-# def test_interstitials_from_potential():
-#     locpot = Locpot.from_file(str(Path(__file__).parent / "vasp_files" / "NaMgF3_LOCPOT"))
-#     interstitials_from_charge_density(locpot)
 
