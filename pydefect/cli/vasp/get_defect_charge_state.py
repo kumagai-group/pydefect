2067c77bd8f7472492b05abc393e1ed6939b7713
yuuukuma <yuuukuma@gmail.com>
2021-04-29 10:09:06 +0900

    Refactor get_defect_charge_state

---
+++ pydefect/cli/vasp/get_defect_charge_state.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020 Kumagai group.
+
+from pymatgen.io.vasp.inputs import Poscar, Incar, Potcar
+
+
+def get_defect_charge_state(poscar: Poscar, potcar: Potcar, incar: Incar):
+    """Get defect charge state from structure, potcar, and NELECT in INCAR. """
+    nelect = incar.get("NELECT", 0)
+    if poscar.site_symbols != potcar.symbols:
+        raise ValueError(f"Sequence of elements in POSCAR {poscar.site_symbols}"
+                         f" and that in POTCAR {potcar.symbols} is different.")
+    num_elect_neutral = sum([num_atom * pot.nelectrons
+                             for num_atom, pot in zip(poscar.natoms, potcar)])
+    excess_num_electrons = int(nelect - num_elect_neutral)
+    return -excess_num_electrons
+
+
