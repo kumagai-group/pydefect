1cac54d40565cd5e773946d7efa6d52d2014d985
yuuukuma <yuuukuma@gmail.com>
2021-05-02 17:28:33 +0900

    Refactor

--- pydefect/analyzer/eigenvalue_plotter.py
+++ pydefect/analyzer/eigenvalue_plotter.py
@@ -53,7 +53,8 @@ class EigenvaluePlotter:
                  ):
 
         self._title = title
-        self._energies_and_occupations = band_edge_orb_infos.energies_and_occupations
+        self._energies_and_occupations = \
+            band_edge_orb_infos.energies_and_occupations
         self._kpt_coords = band_edge_orb_infos.kpt_coords
         self._lowest_band_idx = band_edge_orb_infos.lowest_band_index
         self._supercell_vbm = supercell_vbm
@@ -106,7 +107,8 @@ class EigenvaluePlotlyPlotter(EigenvaluePlotter):
 
         common = {"mode": "markers", "marker_size": 10}
 
-        for spin_idx, eo_by_spin in enumerate(self._energies_and_occupations, 1):
+        for spin_idx, eo_by_spin in \
+                enumerate(self._energies_and_occupations, 1):
             occupied = [[], [], [], []]
             partially_occupied = [[], [], [], []]
             unoccupied = [[], [], [], []]
@@ -144,6 +146,8 @@ class EigenvaluePlotlyPlotter(EigenvaluePlotter):
                           line=dict(color="MediumPurple", width=4, dash="dot"),
                           row=1, col=spin_idx)
 
+            hover_template = '<i>band index</i>: %{text}<br><b>Energy</b>: ' \
+                             '%{y:.2f}<br><b>Occupation</b>: %{customdata:.2f}'
             for points, color, name in \
                     zip([occupied, partially_occupied, unoccupied],
                         ["blue", "green", "red"],
@@ -151,11 +155,9 @@ class EigenvaluePlotlyPlotter(EigenvaluePlotter):
                 fig.add_trace(go.Scatter(x=points[0], y=points[1],
                                          text=points[2],
                                          customdata=points[3],
-                                         hovertemplate=
-                                         '<i>band index</i>: %{text}' +
-                                         '<br><b>Energy</b>: %{y:.2f}' +
-                                         '<br><b>Occupation</b>: %{customdata:.2f}',
-                                         marker_color=color, name=name, **common),
+                                         hovertemplate=hover_template,
+                                         marker_color=color, name=name,
+                                         **common),
                               row=1, col=spin_idx)
 
                 fig.update_xaxes(tickvals=list(range(len(self._kpt_coords))),
@@ -182,7 +184,6 @@ class EigenvalueMplPlotter(EigenvaluePlotter):
         self._add_xticks()
         self._add_band_edges()
         self._set_x_range()
-        # self._set_y_range()
         self._set_labels()
         self._set_title()
         self._set_formatter()
@@ -195,7 +196,8 @@ class EigenvalueMplPlotter(EigenvaluePlotter):
                 for band_idx, eo_by_band in enumerate(eo_by_k_idx):
                     energy, occup = eo_by_band
                     color = "r" if occup > 0.9 else "b" if occup < 0.1 else "g"
-                    ax.scatter([kpt_idx], energy, marker="o", color=color, s=self._mpl_defaults.circle_size)
+                    ax.scatter([kpt_idx], energy, marker="o", color=color,
+                               s=self._mpl_defaults.circle_size)
 
                     try:
                         higher_band_e = eo_by_k_idx[band_idx + 1][0]
@@ -222,10 +224,6 @@ class EigenvalueMplPlotter(EigenvaluePlotter):
     def _set_x_range(self):
         for ax in self.axs:
             ax.set_xlim(-0.5, len(self._kpt_coords) - 0.5)
-
-    # def _set_y_range(self):
-    #     if self._y_range:
-    #         self.plt.ylim(self._y_range[0], self._y_range[1])
 
     def _set_labels(self):
         self.fig.text(0.5, 0, "K-point coords", ha='center',
