6191765b0a69bc4f960f5c8d073df9ee78f95fb9
yuuukuma <yuuukuma@gmail.com>
2021-05-03 09:16:16 +0900

    Refactor

--- pydefect/analyzer/make_band_edge_states.py
+++ pydefect/analyzer/make_band_edge_states.py
@@ -2,16 +2,22 @@
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
 from collections import defaultdict
 from itertools import zip_longest
-from typing import List
+from typing import List, Tuple
 
 import numpy as np
 from pydefect.analyzer.band_edge_states import BandEdgeOrbitalInfos, \
     PerfectBandEdgeState, BandEdgeStates, EdgeInfo, OrbitalInfo, \
     LocalizedOrbital, BandEdgeState
+from pydefect.analyzer.defect_charge_info import DefectChargeInfo
 from pydefect.defaults import defaults
 
 
-def get_similar_orb_idx(orbs: List[OrbitalInfo], edge_info: EdgeInfo):
+def get_similar_orb_idx(orbs: List[OrbitalInfo],
+                        edge_info: EdgeInfo,
+                        reversed: bool = False) -> Tuple[int, float]:
+    if reversed:
+        orbs = orbs[::-1]  # by band
+
     for i, orb in enumerate(orbs):
         if abs(orb.energy - edge_info.energy) < defaults.similar_energy_criterion:
             orb_diff = orbital_diff(orb.orbitals, edge_info.orbitals)
@@ -54,25 +60,21 @@ def get_localized_orbs(orb_info_by_spin: List[List[OrbitalInfo]],
 
 
 def make_band_edge_states(orbital_infos: BandEdgeOrbitalInfos,
-                          p_edge_state: PerfectBandEdgeState) -> BandEdgeStates:
+                          p_edge_state: PerfectBandEdgeState,
+                          defect_charge_info: DefectChargeInfo = None
+                          ) -> BandEdgeStates:
     vbm_k_idx = orbital_infos.kpt_coords.index(p_edge_state.vbm_info.kpt_coord)
     cbm_k_idx = orbital_infos.kpt_coords.index(p_edge_state.cbm_info.kpt_coord)
 
     states = []
+    lowest_idx = orbital_infos.lowest_band_index
     for orb_info_by_spin in orbital_infos.orbital_infos:
-        reversed_orbs = orb_info_by_spin[vbm_k_idx][::-1]  # by band
-        i, v_diff = get_similar_orb_idx(reversed_orbs, p_edge_state.vbm_info)
-        idx = orbital_infos.lowest_band_index + (len(reversed_orbs) - i - 1)
-        vbm_info = EdgeInfo(band_idx=idx,
-                            kpt_coord=p_edge_state.vbm_info.kpt_coord,
-                            orbital_info=reversed_orbs[i])
+        v_diff, vbm_info = get_vbm_info(orb_info_by_spin[vbm_k_idx], lowest_idx,
+                                        p_edge_state.vbm_info)
 
-        orbs = orb_info_by_spin[cbm_k_idx]
-        i, c_diff = get_similar_orb_idx(orbs, p_edge_state.cbm_info)
-        idx = orbital_infos.lowest_band_index + i
-        cbm_info = EdgeInfo(band_idx=idx,
-                            kpt_coord=p_edge_state.cbm_info.kpt_coord,
-                            orbital_info=orbs[i])
+        c_diff, cbm_info = get_cbm_info(orb_info_by_spin[cbm_k_idx], lowest_idx,
+                                        p_edge_state.cbm_info)
+
         loc_idx_range = [vbm_info.band_idx + 1, cbm_info.band_idx - 1]
         localized_orbs = get_localized_orbs(orb_info_by_spin,
                                             loc_idx_range,
@@ -86,6 +88,25 @@ def make_band_edge_states(orbital_infos: BandEdgeOrbitalInfos,
                                     localized_orbitals=localized_orbs))
 
     return BandEdgeStates(states=states)
+
+
+def get_cbm_info(orbs, lowest_band_idx, edge_info):
+    i, diff = get_similar_orb_idx(orbs, edge_info)
+    idx = lowest_band_idx + i
+    info = EdgeInfo(band_idx=idx,
+                        kpt_coord=edge_info.kpt_coord,
+                        orbital_info=orbs[i])
+    return diff, info
+
+
+def get_vbm_info(orbs, lowest_band_idx, edge_info):
+    reversed_orbs = orbs[::-1]  # by band
+    i, diff = get_similar_orb_idx(orbs, edge_info, reversed=True)
+    idx = lowest_band_idx + (len(reversed_orbs) - i - 1)
+    info = EdgeInfo(band_idx=idx,
+                        kpt_coord=edge_info.kpt_coord,
+                        orbital_info=reversed_orbs[i])
+    return diff, info
 
 
 def orbital_diff(orbital_1: dict, orbital_2: dict) -> float:
--- pydefect/tests/analyzer/test_make_band_edge_states.py
+++ pydefect/tests/analyzer/test_make_band_edge_states.py
@@ -6,6 +6,7 @@ import pytest
 from pydefect.analyzer.band_edge_states import EdgeInfo, BandEdgeOrbitalInfos, \
     PerfectBandEdgeState, OrbitalInfo, BandEdgeStates, BandEdgeState, \
     LocalizedOrbital
+from pydefect.analyzer.defect_charge_info import DefectChargeInfo
 from pydefect.analyzer.make_band_edge_states import make_band_edge_states, \
     orbital_diff
 
@@ -22,11 +23,21 @@ def p_edge_state():
 @pytest.fixture
 def orb_infos():
     orbital_infos = [[[
-        OrbitalInfo(energy=-1.1, orbitals={"Mn": [0.5, 0.6, 0.0, 0.0], "O": [0.0, 0.0, 0.0, 0.0]}, occupation=1.0, participation_ratio=0.1),
-        OrbitalInfo(energy=-0.9, orbitals={"Mn": [0.5, 0.7, 0.0, 0.0], "O": [0.0, 0.0, 0.0, 0.0]}, occupation=1.0, participation_ratio=0.1),  # vbm
-        OrbitalInfo(energy=0.0,  orbitals={"Mn": [0.1, 0.2, 0.0, 0.0], "O": [0.3, 0.4, 0.0, 0.0]}, occupation=1.0, participation_ratio=0.1),  # in-gap
-        OrbitalInfo(energy=1.0,  orbitals={"Mn": [0.5, 0.8, 0.0, 0.0], "O": [0.0, 0.0, 0.0, 0.0]}, occupation=0.0, participation_ratio=0.1),  # in-gap
-        OrbitalInfo(energy=1.2,  orbitals={"Mn": [0.0, 0.0, 0.0, 0.0], "O": [0.1, 0.3, 0.0, 0.0]}, occupation=0.0, participation_ratio=0.1)]]]  # cbm
+        OrbitalInfo(energy=-1.1, orbitals={"Mn": [0.5, 0.6, 0.0, 0.0],
+                                           "O": [0.0, 0.0, 0.0, 0.0]},
+                    occupation=1.0, participation_ratio=0.1),
+        OrbitalInfo(energy=-0.9, orbitals={"Mn": [0.5, 0.7, 0.0, 0.0],
+                                           "O": [0.0, 0.0, 0.0, 0.0]},
+                    occupation=1.0, participation_ratio=0.1),  # vbm
+        OrbitalInfo(energy=0.0,  orbitals={"Mn": [0.1, 0.2, 0.0, 0.0],
+                                           "O": [0.3, 0.4, 0.0, 0.0]},
+                    occupation=1.0, participation_ratio=0.1),  # in-gap
+        OrbitalInfo(energy=1.0,  orbitals={"Mn": [0.5, 0.8, 0.0, 0.0],
+                                           "O": [0.0, 0.0, 0.0, 0.0]},
+                    occupation=0.0, participation_ratio=0.1),  # in-gap
+        OrbitalInfo(energy=1.2,  orbitals={"Mn": [0.0, 0.0, 0.0, 0.0],
+                                           "O": [0.1, 0.3, 0.0, 0.0]},
+                    occupation=0.0, participation_ratio=0.1)]]]  # cbm
     return BandEdgeOrbitalInfos(kpt_coords=[(0.0, 0.0, 0.0)],
                                 kpt_weights=[1.0],
                                 orbital_infos=orbital_infos,
@@ -34,26 +45,47 @@ def orb_infos():
                                 fermi_level=0.5)
 
 
-def test_make_band_edge_state_no_in_gap(p_edge_state, orb_infos):
+def test_make_band_edge_state(p_edge_state, orb_infos):
     actual = make_band_edge_states(orb_infos, p_edge_state)
 
     vbm_info = EdgeInfo(band_idx=9, kpt_coord=(0.0, 0.0, 0.0),
-                        orbital_info=OrbitalInfo(energy=-0.9, orbitals={"Mn": [0.5, 0.7, 0.0, 0.0], "O": [0.0, 0.0, 0.0, 0.0]}, occupation=1.0, participation_ratio=0.1))
+                        orbital_info=OrbitalInfo(
+                            energy=-0.9, orbitals={"Mn": [0.5, 0.7, 0.0, 0.0],
+                                                   "O": [0.0, 0.0, 0.0, 0.0]},
+                            occupation=1.0, participation_ratio=0.1))
     cbm_info = EdgeInfo(band_idx=12, kpt_coord=(0.0, 0.0, 0.0),
-                        orbital_info=OrbitalInfo(energy=1.2, orbitals={"Mn": [0.0, 0.0, 0.0, 0.0], "O": [0.1, 0.3, 0.0, 0.0]}, occupation=0.0, participation_ratio=0.1))
-    localized_orb_1 = LocalizedOrbital(band_idx=10, ave_energy=0.0, occupation=1.0,
-                                       orbitals={"Mn": [0.1, 0.2, 0.0, 0.0], "O": [0.3, 0.4, 0.0, 0.0]},
-                                       participation_ratio=0.1)
-    localized_orb_2 = LocalizedOrbital(band_idx=11, ave_energy=1.0, occupation=0.0,
-                                       orbitals={"Mn": [0.5, 0.8, 0.0, 0.0], "O": [0.0, 0.0, 0.0, 0.0]},
-                                       participation_ratio=0.1)
+                        orbital_info=OrbitalInfo(
+                            energy=1.2, orbitals={"Mn": [0.0, 0.0, 0.0, 0.0],
+                                                  "O": [0.1, 0.3, 0.0, 0.0]},
+                            occupation=0.0, participation_ratio=0.1))
+    localized_orb_1 = LocalizedOrbital(
+        band_idx=10, ave_energy=0.0, occupation=1.0,
+        orbitals={"Mn": [0.1, 0.2, 0.0, 0.0], "O": [0.3, 0.4, 0.0, 0.0]},
+        participation_ratio=0.1)
+    localized_orb_2 = LocalizedOrbital(
+        band_idx=11, ave_energy=1.0, occupation=0.0,
+        orbitals={"Mn": [0.5, 0.8, 0.0, 0.0], "O": [0.0, 0.0, 0.0, 0.0]},
+        participation_ratio=0.1)
     expected = BandEdgeStates(
         states=[BandEdgeState(vbm_info=vbm_info,
                               cbm_info=cbm_info,
                               vbm_orbital_diff=0.09999999999999998,
                               cbm_orbital_diff=0.09999999999999998,
-                              localized_orbitals=[localized_orb_1, localized_orb_2])])
+                              localized_orbitals=[localized_orb_1,
+                                                  localized_orb_2])])
     assert actual == expected
+
+#
+# def test_make_band_edge_state_w_defect_charge_info(p_edge_state, orb_infos, mocker):
+#     defect_charge_info = mocker.Mock()
+#     defect_charge_info.localized_orbitals = [[9]]
+#     actual = make_band_edge_states(orb_infos, p_edge_state, defect_charge_info)
+#     expected = EdgeInfo(band_idx=8, kpt_coord=(0.0, 0.0, 0.0),
+#                         orbital_info=OrbitalInfo(
+#                             energy=-1.1, orbitals={"Mn": [0.5, 0.6, 0.0, 0.0],
+#                                                    "O": [0.0, 0.0, 0.0, 0.0]},
+#                             occupation=1.0, participation_ratio=0.1))
+#     assert actual.states[0].vbm_info == expected
 
 
 def test_orbital_diff(mocker):
@@ -65,6 +97,4 @@ def test_orbital_diff(mocker):
     orb_2 = {"O": [0.0, 0.1, 0.0, 0.0]}
     assert orbital_diff(orb_1, orb_2) == 0.2
 
-
-dir_path = Path(__file__).parent / "band_edge_database" / "NaCl"
 
