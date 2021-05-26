55456dac8450bb6066c3ce6bc552dbbcec5a1e4a
yuuukuma <yuuukuma@gmail.com>
2021-05-26 15:49:53 +0900

    Refactor to extract make_single_calc_summary

--- pydefect/analyzer/make_calc_summary.py
+++ pydefect/analyzer/make_calc_summary.py
@@ -14,17 +14,24 @@ def make_calc_summary(
 
     summaries = {}
     for calc_results, entry, str_info in calc_set:
-        atom_io = num_atom_differences(calc_results.structure,
-                                       p_calc_results.structure)
-        relative_energy = calc_results.energy - p_calc_results.energy
-        is_energy_strange = abs(relative_energy) > defaults.abs_strange_energy
-        summaries[entry.full_name] = SingleCalcSummary(
-            charge=entry.charge,
-            atom_io=atom_io,
-            electronic_conv=calc_results.electronic_conv,
-            ionic_conv=calc_results.ionic_conv,
-            is_energy_strange=is_energy_strange,
-            same_config_from_init=str_info.same_config_from_init,
-            defect_type=str_info.defect_type,
-            symm_relation=str_info.symm_relation)
+        summaries[entry.full_name] = \
+            make_single_calc_summary(calc_results, entry, p_calc_results,
+                                     str_info)
     return CalcSummary(single_summaries=summaries)
+
+
+def make_single_calc_summary(calc_results, entry, p_calc_results, str_info):
+    atom_io = num_atom_differences(calc_results.structure,
+                                   p_calc_results.structure)
+    relative_energy = calc_results.energy - p_calc_results.energy
+    is_energy_strange = abs(relative_energy) > defaults.abs_strange_energy
+    return SingleCalcSummary(
+        charge=entry.charge,
+        atom_io=atom_io,
+        electronic_conv=calc_results.electronic_conv,
+        ionic_conv=calc_results.ionic_conv,
+        is_energy_strange=is_energy_strange,
+        same_config_from_init=str_info.same_config_from_init,
+        defect_type=str_info.defect_type,
+        symm_relation=str_info.symm_relation)
+
