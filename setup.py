536db09b91ae92592da54806dc9e73b8641922cc
yuuukuma <yuuukuma@gmail.com>
2021-08-03 15:54:05 +0900

    Add pydefect_print for main command

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
             'pydefect_util = pydefect.cli.main_util:main',
             'pydefect_vasp = pydefect.cli.vasp.main_vasp:main',
             'pydefect_vasp_util = pydefect.cli.vasp.main_vasp_util:main'
+            'pydefect_print = pydefect.cli.vasp.main_print_json:main'
         ]
     }
 )
