41d81715118368b4fe0aa763d8807e1ecc92ec3c
yuuukuma <yuuukuma@gmail.com>
2022-12-08 15:47:55 +0900

    Add pinning_levels.py

---
+++ pydefect/analyzer/pinning_levels.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2022 Kumagai group.
+from pydefect.analyzer.defect_energy import ChargeEnergies
+from tabulate import tabulate
+
+
+def show_pinning_levels(charge_energies: ChargeEnergies):
+
+    result = []
+    for name, charge_energy in charge_energies.charge_energies_dict.items():
+        pin_levels = charge_energy.pinning_level(0, charge_energies.e_max)
+        h_pin, e_pin = "-", "-"
+        if pin_levels[0]:
+            h_pin = f"charge {pin_levels[0][1]}, level {pin_levels[0][0]:.2f}"
+        if pin_levels[1]:
+            e_pin = f"charge {pin_levels[1][1]}, level {pin_levels[1][0]:.2f}"
+        result.append([name, h_pin, e_pin])
+
+    headers = ["defect", "hole pinning", "electron pinning"]
+    return tabulate(result, headers=headers)
---
+++ pydefect/tests/analyzer/test_pinning_levels.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2022 Kumagai group.
+from pydefect.analyzer.defect_energy import DefectEnergySummary, DefectEnergies, \
+    DefectEnergy
+from pydefect.analyzer.pinning_levels import show_pinning_levels
+
+
+def test_show_pinning_levels():
+    defect_energies = DefectEnergies(
+            atom_io={"O": -1},
+            charges=[0, 1, 2],
+            defect_energies=[DefectEnergy(1.0, {"corr": 2.0}, is_shallow=False),
+                             DefectEnergy(2.0, {"corr": 2.0}, is_shallow=False),
+                             DefectEnergy(3.0, {"corr": 2.0}, is_shallow=True)])
+
+    defect_energy_summary = DefectEnergySummary(
+        title="test",
+        defect_energies={"Va_O1": defect_energies},
+        rel_chem_pots={"A": {"O": -5.0}},
+        cbm=2.0, supercell_vbm=-1.0, supercell_cbm=3.0)
+    cbm = defect_energy_summary.cbm
+    charge_energies = defect_energy_summary.charge_energies("A",
+                                                            allow_shallow=True,
+                                                            with_corrections=True,
+                                                            e_range=(0, cbm))
+    actual = show_pinning_levels(charge_energies=charge_energies)
+    expected = """defect    hole pinning          electron pinning
+--------  --------------------  ------------------
+Va_O      charge 1, level 1.00  -"""
+    assert actual == expected
