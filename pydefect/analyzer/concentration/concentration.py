f1c2ed0c36a4323e50cc6fd2d7efbcda902bb5ef
yuuukuma <yuuukuma@gmail.com>
2023-02-01 09:49:28 +0900

    Modify concentration.py

--- pydefect/analyzer/concentration/concentration.py
+++ pydefect/analyzer/concentration/concentration.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2023 Kumagai group.
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 
 import numpy as np
+from monty.json import MSONable
+from tabulate import tabulate
+from vise.util.mix_in import ToJsonFileMixIn
 
 
 @dataclass
-class CarrierConcentration:
+class CarrierConcentration(MSONable):
     """Concentration per cell"""
     p: float
     n: float
+
+    def __str__(self):
+        return f"p: {self.p:.2e}, n: {self.n:.2e}"
 
     @property
     def net_charge(self):
@@ -22,11 +28,22 @@ class CarrierConcentration:
 
 
 @dataclass
-class DefectConcentration:
+class DefectConcentration(MSONable):
     """Concentration per cell"""
     name: str
     charges: List[int]
     concentrations: List[float]
+
+    @property
+    def str_header(self):
+        return [f"{self.name}_{c}" for c in self.charges]
+
+    @property
+    def str_list(self):
+        return [f"{con:.2e}" for con in self.concentrations]
+
+    def __str__(self):
+        return tabulate([self.str_list], headers=self.str_header)
 
     @property
     def net_charge(self):
@@ -43,11 +60,31 @@ class DefectConcentration:
 
 
 @dataclass
-class Concentration:
+class Concentration(MSONable):
     """Concentration per cell"""
     Ef: float
     carrier: CarrierConcentration
     defects: List[DefectConcentration]
+
+    @property
+    def str_header(self):
+        result = ["Ef", "p", "n"]
+        for d in self.defects:
+            result.extend(d.str_header)
+        result.extend(["net charge", "net ratio"])
+        return result
+
+    @property
+    def str_list(self):
+        result = [f"{self.Ef:.2f}",
+                  f"{self.carrier.p:.2e}", f"{self.carrier.n:.2e}"]
+        for d in self.defects:
+            result.extend(d.str_list)
+        result.extend([f"{self.net_charge:.1e}", f"{self.net_abs_ratio:.1e}"])
+        return result
+
+    def __str__(self):
+        return tabulate([self.str_list], headers=self.str_header)
 
     @property
     def net_charge(self):
@@ -65,32 +102,56 @@ class Concentration:
 
 
 @dataclass
-class ConcentrationByFermiLevel:
+class ConcentrationByFermiLevel(MSONable, ToJsonFileMixIn):
     """Concentration per cell"""
     T: float
     concentrations: List[Concentration]
-#    quench_from_T: float = None
+    equilibrium_concentration: Optional[Concentration]
+    T_before_quench: float = None
 
     def __post_init__(self):
         self.concentrations.sort(key=lambda x: x.Ef)
+
+    def __str__(self):
+        result = [f"T: {self.T}"]
+        table = []
+        for c in self.concentrations:
+            table.append(c.str_list)
+        result.append(tabulate(table, c.str_header))
+        result.append("-"*50)
+        if self.equilibrium_concentration:
+            result.append("Equilibrium concentration")
+            result.append(self.equilibrium_concentration.__str__())
+        return "\n".join(result)
 
     @property
     def most_neutral_concentration(self) -> Concentration:
         return min(self.concentrations, key=lambda x: abs(x.net_charge))
 
-    def next_Ef_to_neutral_concentration(self, n_Ef: int = 10) -> List[float]:
+    def next_Ef_to_neutral_concentration(self, n_Ef: int = 10
+                                         ) -> Optional[List[float]]:
         """
-        Return two concentrations that are closest to a charge neutral condition
-        along the negative and positive net charges.
+        Return multiple concentrations that are closest to a charge neutral
+        condition along the negative and positive net charges.
 
         :return:
         """
-        c_minus = min([c for c in self.concentrations if c.net_charge > 0],
-                      key=lambda x: x.net_charge)
-        c_plus = min([c for c in self.concentrations if c.net_charge <= 0],
-                     key=lambda x: abs(x.net_charge))
+        try:
+            c_minus = min([c for c in self.concentrations if c.net_charge > 0],
+                          key=lambda x: x.net_charge)
+            c_plus = min([c for c in self.concentrations if c.net_charge <= 0],
+                         key=lambda x: abs(x.net_charge))
+        except ValueError:
+            return None
+
+        if self.check_end_point(c_minus, c_plus):
+            return None
 
         return np.linspace(
             c_minus.Ef, c_plus.Ef, n_Ef + 1, endpoint=False)[1:].tolist()
+
+    def check_end_point(self, c_minus, c_plus):
+        end_points = [self.concentrations[0], self.concentrations[-1]]
+        return c_minus in end_points or c_plus in end_points
 
 
--- pydefect/tests/analyzer/concentration/test_concentration.py
+++ pydefect/tests/analyzer/concentration/test_concentration.py
@@ -7,17 +7,16 @@ from pydefect.analyzer.concentration.concentration import CarrierConcentration, 
     DefectConcentration, Concentration, ConcentrationByFermiLevel
 
 
-def test_carrier_concentration():
-    concentration = CarrierConcentration(p=1.23*10**15, n=0.23*10**15)
-    assert_almost_equal(concentration.net_charge, 1.0*10**15)
+@pytest.fixture
+def carrier_concentration():
+    return CarrierConcentration(p=1.23*10**15, n=0.23*10**15)
 
 
-def test_defect_concentration():
-    concentration = DefectConcentration(name="Va_O1",
-                                        charges=[0, 1, 2],
-                                        concentrations=[1.0, 2.0, 3.0])
-    assert_almost_equal(concentration.net_charge, 8.0)
-    assert_almost_equal(concentration.total_concentration, 6.0)
+@pytest.fixture
+def defect_concentration():
+    return DefectConcentration(name="Va_Mg1",
+                               charges=[-1, 0, 1, 2],
+                               concentrations=[1.1, 1.0, 2.0, 3.0])
 
 
 @pytest.fixture
@@ -27,34 +26,57 @@ def concentration():
         carrier=CarrierConcentration(p=1.23, n=0.23),
         defects=[DefectConcentration(name="Va_O1",
                                      charges=[0, 1, 2],
-                                     concentrations=[1.0, 2.0, 3.0])])
+                                     concentrations=[1.0, 2.0, 3.0]),
+                 DefectConcentration(name="Va_Mg1",
+                                     charges=[0, 1, 2],
+                                     concentrations=[1.0, 2.0, 3.0]),
+                 ])
+
+
+def test_carrier_concentration_net_charge(carrier_concentration):
+    assert_almost_equal(carrier_concentration.net_charge, 1.0*10**15)
+
+
+def test_carrier_concentration_str(carrier_concentration):
+    print(carrier_concentration)
+
+
+def test_defect_concentration(defect_concentration):
+    assert_almost_equal(defect_concentration.net_charge, 6.9)
+    assert_almost_equal(defect_concentration.abs_charge, 9.1)
+    assert_almost_equal(defect_concentration.total_concentration, 7.1)
+
+
+def test_defect_concentration_str(defect_concentration):
+    print(defect_concentration)
 
 
 def test_concentrations(concentration):
-    assert concentration.net_charge == 9.0
-    assert concentration.abs_charge == 9.46
-    assert concentration.net_abs_ratio == 9.0 / 9.46
+    assert concentration.net_charge == 17.0
+    assert concentration.abs_charge == 17.46
+    assert concentration.net_abs_ratio == 17.0 / 17.46
 
 
-def test_concentration_by_fermi_level(mocker):
-    mock_1 = mocker.Mock(spec=Concentration, autospec=True)
-    mock_2 = mocker.Mock(spec=Concentration, autospec=True)
-    mock_3 = mocker.Mock(spec=Concentration, autospec=True)
-    mock_4 = mocker.Mock(spec=Concentration, autospec=True)
-
-    mock_1.net_charge = 1.0
-    mock_2.net_charge = 0.1
-    mock_3.net_charge = -1.0
-    mock_4.net_charge = -2.0
-
-    mock_1.Ef = 0.0
-    mock_2.Ef = 1.0
-    mock_3.Ef = 2.0
-    mock_4.Ef = 3.0
-
-    con = ConcentrationByFermiLevel(
-        T=300, concentrations=[mock_1, mock_2, mock_3, mock_4])
-    assert con.most_neutral_concentration == mock_2
-    assert con.next_Ef_to_neutral_concentration(3) == [1.25, 1.5, 1.75]
+def test_concentration_str(concentration):
+    print(concentration)
 
 
+@pytest.fixture
+def con_by_Ef(concentration):
+    va_o1 = DefectConcentration("Va_O1", [0], [1.0])
+    c1 = Concentration(0.0, CarrierConcentration(1.0, 0.0), [va_o1])
+    c2 = Concentration(1.0, CarrierConcentration(0.1, 0.0), [va_o1])
+    c3 = Concentration(2.0, CarrierConcentration(0.0, 1.0), [va_o1])
+    c4 = Concentration(3.0, CarrierConcentration(0.0, 2.0), [va_o1])
+    return ConcentrationByFermiLevel(
+        T=300, concentrations=[c1, c2, c3, c4],
+        equilibrium_concentration=concentration)
+
+
+def test_concentration_by_fermi_level(con_by_Ef):
+    assert con_by_Ef.most_neutral_concentration == con_by_Ef.concentrations[1]
+    assert con_by_Ef.next_Ef_to_neutral_concentration(3) == [1.25, 1.5, 1.75]
+
+
+def test_con_by_Ef_str(con_by_Ef):
+    print(con_by_Ef)
