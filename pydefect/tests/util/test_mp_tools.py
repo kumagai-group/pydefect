e621610580dbc7d40f4fe9bd10b5d10d2f350cee
yuuukuma <yuuukuma@gmail.com>
2024-03-28 07:38:32 +0900

    fix MpQuery

--- pydefect/tests/util/test_mp_tools.py
+++ pydefect/tests/util/test_mp_tools.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import pytest
-
-from pydefect.util.mp_tools import elements, MpQuery
-
-
-def test_elements():
-    assert len(elements) == 118
-    assert elements[0] == "H"
-    assert elements[-1] == "Og"
+from pydefect.util.mp_tools import MpQuery
 
 
 def test_mp_actual_query():
@@ -18,21 +10,3 @@ def test_mp_actual_query():
     print(mp_query.materials)
 
 
-def test_mp_query(mocker):
-    mock = mocker.patch("pydefect.util.mp_tools.MPRester")
-    expected = [{'full_formula': 'Mg9'},
-                {'full_formula': 'Mg1O1'},
-                {'full_formula': 'O8'},
-                {'full_formula': 'O2'},
-                {'full_formula': 'O2'},
-                {'full_formula': 'O8'}]
-    mock.return_value.__enter__.return_value.query.return_value = expected
-    element_list = ["Mg", "O"]
-    excluded = list(set(elements) - set(element_list))
-    mp_query = MpQuery(element_list=element_list,
-                       properties=["full_formula"])
-    mock.return_value.__enter__.return_value.query.assert_called_with(
-        criteria={"elements": {"$in": element_list, "$nin": excluded},
-                  "e_above_hull": {"$lte": 1e-5}},
-        properties=["full_formula"])
-    assert mp_query.materials == expected
--- pydefect/util/mp_tools.py
+++ pydefect/util/mp_tools.py
@@ -2,13 +2,10 @@
 
 from typing import List
 
-from pydefect.defaults import defaults
-from pymatgen.core import Element
-from pymatgen.ext.matproj import MPRester, MPRestError
-from vise.util.logger import get_logger
-from itertools import combinations, chain
+from mp_api.client import MPRester
 
-elements = [e.name for e in Element]
+from pydefect.defaults import defaults
+from vise.util.logger import get_logger
 
 
 logger = get_logger(__name__)
@@ -22,39 +19,14 @@ class MpQuery:
         # API key is parsed via .pmgrc.yaml
         with MPRester() as m:
             # Due to mp_decode=True by default, class objects are restored.
-            excluded = list(set(elements) - set(element_list))
-            try:
-                default_properties = ["task_id", "full_formula", "final_energy",
-                                      "structure", "spacegroup", "band_gap",
-                                      "total_magnetization", "magnetic_type"]
-                criteria = (
-                    {"elements": {"$in": element_list, "$nin": excluded},
-                     "e_above_hull": {"$lte": e_above_hull}})
-                self.materials = m.query(
-                    criteria=criteria,
-                    properties=properties or default_properties)
-            except:
-                logger.info("Note that you're using the newer MPRester.")
-                default_fields = ["material_id", "formula_pretty", "structure",
-                                  "symmetry", "band_gap", "total_magnetization",
-                                  "types_of_magnetic_species"]
-                properties = properties or default_fields
-                self.materials = m.materials.summary.search(
-                    exclude_elements=excluded,
-                    energy_above_hull=(-1e-5, e_above_hull),
-                    fields=properties)
-
-
-class MpEntries:
-    def __init__(self,
-                 element_list: List[str],
-                 e_above_hull: float = defaults.e_above_hull,
-                 additional_properties: List[str] = None):
-        excluded = list(set(elements) - set(element_list))
-        criteria = ({"elements": {"$in": element_list, "$nin": excluded},
-                     "e_above_hull": {"$lte": e_above_hull}})
-        with MPRester() as m:
-            self.materials = m.get_entries(
-                criteria, property_data=additional_properties)
+            logger.info("Note that you're using the newer MPRester.")
+            default_fields = ["material_id", "formula_pretty", "structure",
+                              "symmetry", "band_gap", "total_magnetization",
+                              "types_of_magnetic_species"]
+            properties = properties or default_fields
+            self.materials = m.materials.summary.search(
+                chemsys=element_list,
+                energy_above_hull=(-1e-5, e_above_hull),
+                fields=properties)
 
 
